cc4b4f7aadefc342252a27bac9e21092ab2e6166
flipvanhaaren <flip@shareworks.nl>
2024-01-23 13:32:55 +0100

    update ts config emails

--- emails/emails/reset-password.tsx
+++ emails/emails/reset-password.tsx
@@ -1,7 +1,7 @@
 import { Body, Button, Container, Head, Html, Img, Link, Preview, Section, Tailwind, Text } from '@react-email/components';
 
-import config from '../../config';
-import { getI18n } from '../../i18n';
+import config from 'config';
+import { getI18n } from 'i18n';
 
 interface Props {
   username?: string;
