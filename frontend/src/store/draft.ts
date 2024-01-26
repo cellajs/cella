f7f737ff78b2e67738e9fdfd4689d4b64221b245
Roman <nabukhotnyiroman@gmail.com>
2024-01-26 13:10:33 +0200

    fix: Fix imports and seeds

--- frontend/src/store/draft.ts
+++ frontend/src/store/draft.ts
@@ -2,7 +2,7 @@ import { config } from 'config';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-import type { } from '@redux-devtools/extension';
+import type {} from '@redux-devtools/extension';
 
 import { immer } from 'zustand/middleware/immer';
 
