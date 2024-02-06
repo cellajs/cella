35610085742af4ca0f87dc418196b263a6195722
flipvanhaaren <flip@shareworks.nl>
2024-02-06 17:30:34 +0100

    breadcrumb

--- frontend/src/components/page-header.tsx
+++ frontend/src/components/page-header.tsx
@@ -1,5 +1,7 @@
 import { AvatarWrap } from '~/components/avatar-wrap';
+import { Breadcrumb, BreadcrumbItem } from '~/components/ui/breadcrumb';
 
+// PageHeaderProps Interface
 interface PageHeaderProps {
   title: string;
   type: 'user' | 'organization';
@@ -18,13 +20,22 @@ const PageHeader = ({ title, avatar, bannerUrl, type, panel }: PageHeaderProps) 
 
   return (
     <div className="relative">
+      {/* Banner */}
       <div className={`bg-gray-400/25 bg-cover bg-center ${bannerHeight}`} style={bannerUrl ? { backgroundImage: `url(${bannerUrl})` } : {}} />
       <div className="absolute flex bottom-0 w-full bg-background/50 backdrop-blur-sm">
         <div className="flex items-stretch">
           {avatar && <AvatarWrap className="m-2" type={type} id={avatar.id} name={avatar.name} url={avatar.thumbnailUrl} />}
           <div className="my-auto">
+
+            {/* Page title */}
             <h1 className="text-xl leading-5 font-semibold">{title}</h1>
-            <strong className="text-sm font-light">{type}</strong>
+
+            {/* Breadcrumb */}
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <strong className="text-sm font-light">{type}</strong>
+              </BreadcrumbItem>
+            </Breadcrumb>
           </div>
         </div>
         <div className="flex ml-auto items-center">{panel}</div>
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import calendar from 'dayjs/plugin/calendar';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { twMerge } from 'tailwind-merge';
+import * as React from "react"
 
 dayjs.extend(calendar);
 dayjs.extend(relativeTime);
@@ -33,6 +34,12 @@ export function dateShort(date?: string | null) {
     lastWeek: '[Last] dddd',
     sameElse: 'DD/MM/YYYY',
   });
+}
+
+export function getValidChildren(children: React.ReactNode) {
+  return React.Children.toArray(children).filter((child) =>
+    React.isValidElement(child)
+  ) as React.ReactElement[]
 }
 
 // export function getObjectWithArrayForEachKey<T>(keys: string[]): Record<string, T[]> {
