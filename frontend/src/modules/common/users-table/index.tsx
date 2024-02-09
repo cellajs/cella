64ca0adf1472d4b75eb734243149e380beb1f7cd
Roman <nabukhotnyiroman@gmail.com>
2024-02-09 14:28:12 +0200

    feat: Add accordion effect for expand row

--- frontend/src/modules/common/users-table/index.tsx
+++ frontend/src/modules/common/users-table/index.tsx
@@ -133,19 +133,36 @@ const UsersTable = () => {
     setRole(undefined);
   };
 
-  const onRowsChange = (rows: UserRow[], { indexes }: RowsChangeData<UserRow>) => {
+  const onRowsChange = (changedRows: UserRow[], { indexes }: RowsChangeData<UserRow>) => {
+    let rows = [...changedRows];
     const row = rows[indexes[0]];
 
     if (row.type === 'MASTER') {
       if (row.expanded) {
+        const detailId = `${row.id}-detail`;
         rows.splice(indexes[0] + 1, 0, {
           type: 'DETAIL',
-          id: `${row.id}-detail`,
+          id: detailId,
           parent: row,
         });
+
+        // Close other masters
+        rows = rows.map((r) => {
+          if (r.type === 'MASTER' && r.id === row.id) {
+            return r;
+          }
+          return {
+            ...r,
+            expanded: false,
+          };
+        });
+
+        // Remove other details
+        rows = rows.filter((r) => r.type === 'MASTER' || r.id === detailId);
       } else {
         rows.splice(indexes[0] + 1, 1);
       }
+
       setRows(rows);
     }
   };
