3be7e438167e70fa87d7d5ff6de9be79385de359
Roman <nabukhotnyiroman@gmail.com>
2024-02-15 11:06:08 +0200

    feat: Update organization route

--- frontend/src/modules/organizations/members-table/index.tsx
+++ frontend/src/modules/organizations/members-table/index.tsx
@@ -10,10 +10,10 @@ import { Bird } from 'lucide-react';
 import { SortColumn } from 'react-data-grid';
 import useMutateQueryData from '~/hooks/use-mutate-query-data';
 import { OrganizationContext } from '~/modules/organizations/organization';
-import { MembersSearch, OrganizationRoute } from '~/router/routeTree';
 import useSaveInSearchParams from '../../../hooks/use-save-in-search-params';
 import { useColumns } from './columns';
 import Toolbar from './toolbar';
+import { MembersSearch, OrganizationRoute } from '~/router/routeTree';
 
 const MembersTable = () => {
   const { organization } = useContext(OrganizationContext);
