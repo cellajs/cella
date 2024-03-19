818d884c994fd2c899b5b37363ca2530907dc600
yossydev <codeyy021@gmail.com>
2024-03-20 00:41:07 +0900

    feat(blog): building a blog environment using honox

---
+++ blog/vite.config.ts
@@ -0,0 +1,15 @@
+import pages from "@hono/vite-cloudflare-pages";
+import honox from "honox/vite";
+import client from "honox/vite/client";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  }
+  return {
+    plugins: [honox(), pages()],
+  };
+});
