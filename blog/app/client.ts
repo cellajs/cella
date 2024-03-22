195d149523b4c5821e787f239b88d3e2417d6589
yossydev <codeyy021@gmail.com>
2024-03-22 16:50:28 +0900

    feat: use react-renderer to make shadcn/ui work

--- blog/app/client.ts
+++ blog/app/client.ts
@@ -1,3 +1,12 @@
-import { createClient } from 'honox/client'
+import { createClient } from "honox/client";
 
-createClient()
+createClient({
+  hydrate: async (elem, root) => {
+    const { hydrateRoot } = await import("react-dom/client");
+    hydrateRoot(root, elem);
+  },
+  createElement: async (type: any, props: any) => {
+    const { createElement } = await import("react");
+    return createElement(type, props);
+  },
+});
--- blog/app/routes/index.tsx
+++ blog/app/routes/index.tsx
@@ -1,18 +1,13 @@
-import { css } from 'hono/css'
-import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
-
-const className = css`
-  font-family: sans-serif;
-`
+import { createRoute } from "honox/factory";
+import Counter from "../islands/counter";
 
 export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'Hono'
+  const name = c.req.query("name") ?? "Hono";
   return c.render(
-    <div class={className}>
+    <div>
       <h1>Hello, {name}!</h1>
       <Counter />
     </div>,
-    { title: name }
-  )
-})
+    { title: name },
+  );
+});
