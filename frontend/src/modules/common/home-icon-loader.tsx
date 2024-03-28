235c7089f9897f9578372c778b86b7b3cfae37d6
flipvanhaaren <flip@shareworks.nl>
2024-03-28 13:25:02 +0100

    useIsFetching for app-nav spinner

--- frontend/src/modules/common/home-icon-loader.tsx
+++ frontend/src/modules/common/home-icon-loader.tsx
@@ -5,10 +5,15 @@ import useAppState from '~/hooks/use-app-state';
 import router from '~/router';
 import { useNavigationStore } from '~/store/navigation';
 import Logo from './logo';
+import { useIsFetching } from '@tanstack/react-query';
 
 const HomeIconLoader = () => {
   const { isReady } = useAppState();
   const { navLoading, setLoading } = useNavigationStore();
+  const isFetching = useIsFetching();
+
+  // Show loading spinner when fetching data or navigating
+  const isLoading = isFetching > 0 || navLoading;
 
   useEffect(() => {
     router.subscribe('onBeforeLoad', ({ pathChanged }) => {
@@ -23,13 +28,13 @@ const HomeIconLoader = () => {
     <>
       <Logo
         iconOnly
-        className={`${navLoading && 'animate-spin-slow'} w-8 saturate-[.9] group-hover:scale-110 absolute transition-all group-hover:opacity-0 -z-0 ${
-          isReady && !navLoading && 'ease-in-out opacity-0 scale-0'
+        className={`${isLoading && 'animate-spin-slow'} w-8 saturate-[.9] group-hover:scale-110 absolute transition-all group-hover:opacity-0 -z-0 ${
+          isReady && !isLoading && 'ease-in-out opacity-0 scale-0'
         }`}
       />
       <Home
         strokeWidth={config.theme.strokeWidth}
-        className={`transition-all ease-in-out group-hover:scale-110 group-hover:opacity-100 ${!isReady || navLoading ? 'opacity-0 scale-0' : ''}`}
+        className={`transition-all ease-in-out group-hover:scale-110 group-hover:opacity-100 ${!isReady || isLoading ? 'opacity-0 scale-0' : ''}`}
       />
     </>
   );
