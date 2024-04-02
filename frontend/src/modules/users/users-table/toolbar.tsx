efaf07223d6573a7b5c66975720e7c4291e8db47
David Hordiienko <neonz96@mail.ru>
2024-04-02 16:03:48 +0300

    add: filter

--- frontend/src/modules/users/users-table/toolbar.tsx
+++ frontend/src/modules/users/users-table/toolbar.tsx
@@ -1,9 +1,8 @@
 import { Mail, Trash, XSquare } from 'lucide-react';
-import { type Dispatch, type SetStateAction, useRef } from 'react';
+import { type Dispatch, type SetStateAction, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
 import type { GetUsersParams } from '~/api/users';
-import TableSearch from '~/modules/common/data-table/table-search';
 import { FocusView } from '~/modules/common/focus-view';
 import InviteUsers from '~/modules/common/invite-users';
 import { Badge } from '~/modules/ui/badge';
@@ -16,6 +15,8 @@ import ColumnsView, { type ColumnOrColumnGroup } from '../../common/data-table/c
 import TableCount from '../../common/data-table/table-count';
 import { dialog } from '../../common/dialoger/state';
 import DeleteUsers from '../delete-users';
+import TableSearch from '~/modules/common/data-table/table-search';
+import { useSize } from '~/hooks/use-size';
 
 interface Props {
   total?: number;
@@ -57,6 +58,12 @@ function Toolbar({
 
   const containerRef = useRef(null);
 
+  const windowSize = useSize();
+
+  const [isFilterOpen, setFilterOpen] = useState<boolean>(role !== undefined || query !== undefined ? true : false);
+
+  const [isButtonClicked, setButtonClicked] = useState<boolean>(role !== undefined || query !== undefined ? true : false);
+
   const openInviteDialog = () => {
     dialog(<InviteUsers mode="email" dialog />, {
       drawerOnMobile: false,
@@ -85,11 +92,67 @@ function Toolbar({
       },
     );
   };
+  const onShowFilterClick = () => {
+    setButtonClicked(true);
+    setFilterOpen(true);
+  };
 
+  const onFiltersHideClick = () => {
+    setButtonClicked(false);
+    setFilterOpen(false);
+    if (onResetFilters) onResetFilters();
+  };
+
+  const crossButton = useMemo(() => {
+    if (windowSize.width < 640 && isFilterOpen) return <Button onClick={onFiltersHideClick}>X</Button>;
+  }, [isFilterOpen, windowSize.width]);
+
+  const filters = useMemo(() => {
+    if (!isFilterOpen) return <Button onClick={onShowFilterClick}>Filter</Button>;
+    return (
+      <>
+        <TableSearch query={query} setQuery={setQuery} />
+        <Select
+          value={role === undefined ? 'all' : role}
+          onValueChange={(role) => {
+            setRole(role === 'all' ? undefined : (role as GetUsersParams['role']));
+          }}
+        >
+          <SelectTrigger className="h-10 w-[150px]">
+            <SelectValue placeholder={t('common:placeholder.select_role')} />
+          </SelectTrigger>
+          <SelectContent>
+            {items.map(({ key, value }) => (
+              <SelectItem key={key} value={key}>
+                {t(value)}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </>
+    );
+  }, [isFilterOpen, query, role]);
+
+  const isFiltersShown = useMemo(() => {
+    return (windowSize.width < 640 && !isFilterOpen) || windowSize.width >= 640;
+  }, [windowSize.width, isFilterOpen]);
+
+  useEffect(() => {
+    (() => {
+      if (windowSize.width >= 640 && !isFilterOpen) {
+        setFilterOpen(true);
+        return;
+      }
+      if (windowSize.width < 640 && !isButtonClicked && isFilterOpen) {
+        setFilterOpen(false);
+        return;
+      }
+    })();
+  }, [windowSize]);
   return (
     <>
-      <div className="items-center justify-between sm:flex">
-        <div className="flex items-center space-x-2">
+      <div className={`items-center flex justify-${isFiltersShown ? 'between' : 'center'}`}>
+        <div className={`${isFiltersShown ? 'flex' : 'hidden'} items-center space-x-2`}>
           {selectedUsers.length > 0 ? (
             <>
               <Button variant="destructive" onClick={openDeleteDialog} className="relative">
@@ -114,24 +177,8 @@ function Toolbar({
           {selectedUsers.length === 0 && <TableCount count={total} type="user" isFiltered={isFiltered} onResetFilters={onResetFilters} />}
         </div>
         <div className="mt-2 flex items-center space-x-2 sm:mt-0">
-          <TableSearch query={query} setQuery={setQuery} />
-          <Select
-            value={role === undefined ? 'all' : role}
-            onValueChange={(role) => {
-              setRole(role === 'all' ? undefined : (role as GetUsersParams['role']));
-            }}
-          >
-            <SelectTrigger className="h-10 w-[150px]">
-              <SelectValue placeholder={t('common:placeholder.select_role')} />
-            </SelectTrigger>
-            <SelectContent>
-              {items.map(({ key, value }) => (
-                <SelectItem key={key} value={key}>
-                  {t(value)}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+          {filters}
+          {crossButton}
           <ColumnsView className="max-lg:hidden" columns={columns} setColumns={setColumns} />
           <FocusView iconOnly />
         </div>
