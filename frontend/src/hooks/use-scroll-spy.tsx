3fb4358d836e16708019408335aa6ba9aa605416
David Hordiienko <neonz96@mail.ru>
2024-04-03 10:40:04 +0300

    add: sign up  config option

---
+++ frontend/src/hooks/use-scroll-spy.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import throttle from 'lodash/fp/throttle';
+
+export interface useScrollSpyParams {
+  activeSectionDefault?: number;
+  offsetPx?: number;
+  sectionElementRefs: React.RefObject<HTMLElement>[];
+  throttleMs?: number;
+  scrollingElement?: React.RefObject<HTMLElement>;
+}
+export const useScrollSpy = ({
+  activeSectionDefault = 0,
+  offsetPx = 0,
+  scrollingElement,
+  sectionElementRefs = [],
+  throttleMs = 100,
+}: useScrollSpyParams): number | undefined => {
+  const [activeSection, setActiveSection] = useState(activeSectionDefault);
+
+  const handle = throttle(throttleMs, () => {
+    let currentSectionId = activeSection;
+    for (let i = 0; i < sectionElementRefs.length; i += 1) {
+      const section = sectionElementRefs[i].current;
+      // Needs to be a valid DOM Element
+      if (!section || !(section instanceof Element)) continue;
+      // GetBoundingClientRect returns values relative to viewport
+      if (section.getBoundingClientRect().top + offsetPx < 0) {
+        currentSectionId = i;
+        continue;
+      }
+      // No need to continue loop, if last element has been detected
+      break;
+    }
+
+    setActiveSection(currentSectionId);
+  });
+
+  useEffect(() => {
+    const scrollable = scrollingElement?.current ?? window;
+    scrollable.addEventListener('scroll', handle);
+
+    // Run initially
+    handle();
+
+    return () => {
+      scrollable.removeEventListener('scroll', handle);
+    };
+  }, [sectionElementRefs, offsetPx, scrollingElement, handle]);
+  return activeSection;
+};
--- frontend/src/modules/auth/sign-in.tsx
+++ frontend/src/modules/auth/sign-in.tsx
@@ -6,13 +6,17 @@ import { SignUpForm } from './sign-up-form';
 import { useUserStore } from '~/store/user';
 import AuthPage from '.';
 import OauthOptions from './oauth-options';
+import { Button } from '../ui/button';
+import { useNavigate } from '@tanstack/react-router';
 
-type Step = 'check' | 'signIn' | 'signUp';
+type Step = 'check' | 'signIn' | 'signUp' | 'inviteOnly';
 
 const SignIn = () => {
   const { lastUser } = useUserStore();
   const [step, setStep] = useState<Step>('check');
   const [email, setEmail] = useState('');
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (lastUser?.email) handleCheckEmail('signIn', lastUser.email);
@@ -32,8 +36,16 @@ const SignIn = () => {
       {step === 'check' && <CheckEmailForm setStep={handleCheckEmail} />}
       {step === 'signIn' && <SignInForm email={email} setStep={handleSetStep} />}
       {step === 'signUp' && <SignUpForm email={email} setStep={handleSetStep} />}
-
-      <OauthOptions actionType={step} />
+      {step === 'inviteOnly' && (
+        <>
+          <h1 className="text-2xl text-center pb-2 mt-4">Hey there!</h1>
+          <h2 className="text-2xl text-center pb-4 mt-4">Cella is currently invite-only. Thanks for your interest!</h2>
+          <Button type="submit" className="w-full" onClick={async () => await navigate({ to: '/', replace: true })}>
+            About page
+          </Button>
+        </>
+      )}
+      {step !== 'inviteOnly' && <OauthOptions actionType={step} />}
     </AuthPage>
   );
 };
