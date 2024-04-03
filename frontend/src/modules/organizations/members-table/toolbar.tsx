b58c028a923c8c0bcfcb9cfe043da93c981b8984
Flip van Haaren <flip@shareworks.nl>
2024-04-03 10:50:53 +0200

    Feture/search&filters mobile (#18)
    * add: filter
    
    * add: icon on filter btn
    
    * +
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * refactor expand component
    
    * downtime alert
    
    * status url in offline alert
    
    * feat: Add sessions section in profile settings
    
    * disable console.debug in prod
    
    * feat: tooltip added to focus view (#14)
    
    * feat: tooltip added to focus view
    
    * tooltip custom component
    
    * tooltip-button absolute import
    
    * update deps
    
    * add: sign up  config option
    
    * translation fixes, remove lodash
    
    * remove lodash
    
    ---------
    
    Co-authored-by: David Hordiienko <neonz96@mail.ru>
    Co-authored-by: David <90209095+LemonardoD@users.noreply.github.com>
    Co-authored-by: Roman <nabukhotnyiroman@gmail.com>
    Co-authored-by: Aashish Upadhyay <81024263+Aashish-Upadhyay-101@users.noreply.github.com>

--- frontend/src/modules/organizations/members-table/toolbar.tsx
+++ frontend/src/modules/organizations/members-table/toolbar.tsx
@@ -1,11 +1,10 @@
 import { Mail, Trash, XSquare } from 'lucide-react';
-import { type Dispatch, type SetStateAction, useContext, useRef } from 'react';
+import { type Dispatch, type SetStateAction, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
 import { type GetMembersParams, getMembersByOrganizationIdentifier } from '~/api/organizations';
 import { cn } from '~/lib/utils';
 import Export from '~/modules/common/data-table/export';
-import TableSearch from '~/modules/common/data-table/table-search';
 import { FocusView } from '~/modules/common/focus-view';
 import InviteUsers from '~/modules/common/invite-users';
 import { Badge } from '~/modules/ui/badge';
@@ -19,6 +18,9 @@ import TableCount from '../../common/data-table/table-count';
 import { dialog } from '../../common/dialoger/state';
 import { OrganizationContext } from '../organization';
 import RemoveMembersForm from './remove-member-form';
+import TableSearch from '~/modules/common/data-table/table-search';
+import { useSize } from '~/hooks/use-size';
+import { Filter } from 'lucide-react';
 
 interface Props {
   selectedMembers: Member[];
@@ -65,6 +67,11 @@ function Toolbar({
   const user = useUserStore((state) => state.user);
 
   const containerRef = useRef(null);
+  const windowSize = useSize();
+
+  const [isFilterOpen, setFilterOpen] = useState<boolean>(role !== undefined || query !== undefined ? true : false);
+
+  const [isButtonClicked, setButtonClicked] = useState<boolean>(role !== undefined || query !== undefined ? true : false);
 
   const openInviteDialog = () => {
     dialog(<InviteUsers organization={organization} dialog />, {
@@ -102,10 +109,74 @@ function Toolbar({
     );
   };
 
+  const onShowFilterClick = () => {
+    setButtonClicked(true);
+    setFilterOpen(true);
+  };
+
+  const onFiltersHideClick = () => {
+    setButtonClicked(false);
+    setFilterOpen(false);
+    if (onResetFilters) onResetFilters();
+  };
+
+  const crossButton = useMemo(() => {
+    if (windowSize.width < 640 && isFilterOpen) return <Button onClick={onFiltersHideClick}>X</Button>;
+  }, [isFilterOpen, windowSize.width]);
+
+  const filters = useMemo(() => {
+    if (!isFilterOpen)
+      return (
+        <Button className="mt-0" onClick={onShowFilterClick}>
+          <Filter width={16} height={16} />
+          <span className="ml-1">Filter</span>
+        </Button>
+      );
+    return (
+      <>
+        <TableSearch query={query} setQuery={setQuery} />
+        <Select
+          value={role === undefined ? 'all' : role}
+          onValueChange={(role) => {
+            setRole(role === 'all' ? undefined : (role as GetMembersParams['role']));
+          }}
+        >
+          <SelectTrigger className={cn('h-10 w-[125px]', role !== undefined && 'text-primary')}>
+            <SelectValue placeholder={t('common:placeholder.select_role')} />
+          </SelectTrigger>
+          <SelectContent>
+            {items.map(({ key, value }) => (
+              <SelectItem key={key} value={key}>
+                {t(value)}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </>
+    );
+  }, [isFilterOpen, query, role]);
+
+  const isFiltersShown = useMemo(() => {
+    return (windowSize.width < 640 && !isFilterOpen) || windowSize.width >= 640;
+  }, [windowSize.width, isFilterOpen]);
+
+  useEffect(() => {
+    (() => {
+      if (windowSize.width >= 640 && !isFilterOpen) {
+        setFilterOpen(true);
+        return;
+      }
+      if (windowSize.width < 640 && !isButtonClicked && isFilterOpen) {
+        setFilterOpen(false);
+        return;
+      }
+    })();
+  }, [windowSize]);
+
   return (
     <>
-      <div className="items-center justify-between sm:flex">
-        <div className="flex items-center space-x-2">
+      <div className={`items-center flex justify-${isFiltersShown ? 'between' : 'center'}`}>
+        <div className={`${isFiltersShown ? 'flex' : 'hidden'} items-center space-x-2`}>
           {selectedMembers.length > 0 ? (
             <>
               <Button variant="destructive" onClick={openRemoveDialog} className="relative">
@@ -114,7 +185,7 @@ function Toolbar({
                 <span className="ml-1 max-xs:hidden">{t('common:remove')}</span>
               </Button>
               <Button variant="ghost" onClick={onResetSelectedRows}>
-                <XSquare size={16} />
+                <XSquare size={42} />
                 <span className="ml-1">{t('common:clear')}</span>
               </Button>
             </>
@@ -130,27 +201,9 @@ function Toolbar({
           {selectedMembers.length === 0 && <TableCount count={total} type="member" isFiltered={isFiltered} onResetFilters={onResetFilters} />}
         </div>
         <div className="mt-2 flex items-center space-x-2 sm:mt-0">
-          <TableSearch query={query} setQuery={setQuery} />
-          <Select
-            value={role === undefined ? 'all' : role}
-            onValueChange={(role) => {
-              setRole(role === 'all' ? undefined : (role as GetMembersParams['role']));
-            }}
-          >
-            <SelectTrigger className={cn('h-10 w-[125px]', role !== undefined && 'text-primary')}>
-              <SelectValue placeholder={t('common:placeholder.select_role')} />
-            </SelectTrigger>
-            <SelectContent>
-              {items.map(({ key, value }) => (
-                <SelectItem key={key} value={key}>
-                  {t(value)}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-
+          {filters}
+          {crossButton}
           <ColumnsView className="max-lg:hidden" columns={columns} setColumns={setColumns} />
-
           <Export
             className="max-lg:hidden"
             filename={`${organization.slug}-members`}
