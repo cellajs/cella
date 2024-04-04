572de6d23c4d9eb43f6a7e3f607fc95f19d3a3fe
Roman <nabukhotnyiroman@gmail.com>
2024-04-04 12:25:52 +0300

    fix: Fix draft form hook

--- frontend/src/store/draft.ts
+++ frontend/src/store/draft.ts
@@ -2,8 +2,6 @@ import { config } from 'config';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-import type {} from '@redux-devtools/extension';
-
 import { immer } from 'zustand/middleware/immer';
 
 interface DraftState {
