e10d638f3554eaa604901753c9f8223be246d8b7
flipvanhaaren <flip@shareworks.nl>
2024-04-05 22:50:19 +0200

    fix sse

--- frontend/src/modules/common/sse/use-sse.tsx
+++ frontend/src/modules/common/sse/use-sse.tsx
@@ -4,15 +4,13 @@ import { SSEContext } from './provider';
 export function useSSE(eventName: string, listener: (this: EventSource, event: MessageEvent) => unknown): void {
   const source = useContext(SSEContext);
 
-  if (!source) {
-    throw new Error('Could not find an SSE context; You have to wrap useSSE() in a <SSEProvider>.');
-  }
-
   useEffect(() => {
+    if (!source) return;
+
     source.addEventListener(eventName, listener);
 
     return () => {
       source.removeEventListener(eventName, listener);
     };
-  }, []);
+  }, [source]);
 }
