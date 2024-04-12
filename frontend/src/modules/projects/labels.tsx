4f140f7bdb220aba6dab9c8a2e855f5513c4f7a7
David Hordiienko <neonz96@mail.ru>
2024-04-12 15:22:23 +0300

    fix: labels

--- frontend/src/modules/projects/labels.tsx
+++ frontend/src/modules/projects/labels.tsx
@@ -2,12 +2,10 @@ import { Check } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '~/lib/utils';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '~/modules/ui/accordion';
 import { Badge } from '~/modules/ui/badge';
 import { Button } from '~/modules/ui/button';
-import { Command, CommandGroup, CommandInput, CommandItem } from '~/modules/ui/command';
+import { Command, CommandInput, CommandItem, CommandSeparator } from '~/modules/ui/command';
 import { Input } from '~/modules/ui/input';
-import { Label } from '~/modules/ui/label';
 import { Popover, PopoverContent, PopoverTrigger } from '~/modules/ui/popover';
 import { PopoverPortal } from '@radix-ui/react-popover';
 import { ScrollArea } from '../ui/scroll-area';
@@ -60,7 +58,7 @@ export const LabelBox = () => {
   const [isOpenEditLabel, setOpenEditLabel] = React.useState(false);
   const [searchValue, setSearchValue] = React.useState<string>('');
   const [selectedLabels, setSelectedLabels] = React.useState<LabelType[]>([Labels[0], Labels[1], Labels[2]]);
-  const [accordionValue, setAccordionValue] = React.useState<string>('');
+  const [editedValue, setEditedValue] = React.useState<string>('');
 
   const createLabel = (name: string) => {
     const newLabel = {
@@ -109,7 +107,7 @@ export const LabelBox = () => {
     <>
       <Popover open={isOpenEditLabel} onOpenChange={onComboboxOpenChange}>
         <PopoverTrigger asChild>
-          <div className="relative flex align-center justify-start overflow-hidden gap-1 ">
+          <div className="relative w-full min-h-[22px] flex align-center justify-start overflow-hidden gap-1">
             {!isOpenEditLabel &&
               selectedLabels.map(({ label, value, color }) => (
                 <button type="button" onClick={() => setOpenEditLabel(true)}>
@@ -135,31 +133,34 @@ export const LabelBox = () => {
                 setZeroValue={setSearchValue}
                 onValueChange={setSearchValue}
               />
-              <CommandGroup>
+              <div className="pt-2.5 pb-2.5 pl-0.5 pr-0.5">
                 <CommandList>
                   <ScrollArea className="h-[200px] overflow-y-auto pl-2 pr-3">
-                    <div className="flex-1 min-w-[340px]">
+                    <div className="flex-1 min-w-[375px]">
                       {labels
-                        .filter((label) => label.label.toLowerCase().includes(searchValue.toLowerCase()))
+                        .filter((label) => label.value.includes(searchValue.toLowerCase()))
                         .map((label) => {
                           const isActive = selectedLabels.includes(label);
                           return (
-                            <LabelsListItem
-                              accordionValue={accordionValue}
-                              setAccordionValue={setAccordionValue}
-                              onSelect={() => toggleLabel(label)}
-                              isActive={isActive}
-                              onDelete={() => deleteLabel(label)}
-                              onSubmit={(e) => submitLabelItemClick(e, label)}
-                              {...label}
-                            />
+                            <>
+                              <LabelsListItem
+                                editedValue={editedValue}
+                                setEditedValue={setEditedValue}
+                                onSelect={() => toggleLabel(label)}
+                                isActive={isActive}
+                                onDelete={() => deleteLabel(label)}
+                                onSubmit={(e) => submitLabelItemClick(e, label)}
+                                {...label}
+                              />
+                              <CommandSeparator />
+                            </>
                           );
                         })}
                     </div>
                   </ScrollArea>
-                  <CreateLabel onSelect={() => createLabel(searchValue)} {...{ searchValue, labels }} />
+                  <CreateLabel setDefaultSearch={setSearchValue} onSelect={() => createLabel(searchValue)} {...{ searchValue, labels }} />
                 </CommandList>
-              </CommandGroup>
+              </div>
             </Command>
           </PopoverContent>
         </PopoverPortal>
@@ -172,9 +173,10 @@ type CreateLabelType = {
   searchValue: string;
   labels: LabelType[];
   onSelect: () => void;
+  setDefaultSearch: (value: '') => void;
 };
 
-const CreateLabel = ({ searchValue, labels, onSelect }: CreateLabelType) => {
+const CreateLabel = ({ searchValue, setDefaultSearch, labels, onSelect }: CreateLabelType) => {
   const hasNoLabel = !labels.some((label) => label.value === searchValue.toLowerCase());
   const render = searchValue !== '' && hasNoLabel;
   if (!render) return null;
@@ -189,7 +191,7 @@ const CreateLabel = ({ searchValue, labels, onSelect }: CreateLabelType) => {
           className="aria-selected:bg-transparent mt-2 text-xs text-muted-foreground"
           onSelect={onSelect}
         >
-          <Button className="w-full" variant={'outlineGhost'} size={'xs'}>
+          <Button onClick={() => setDefaultSearch('')} className="w-full" variant={'outlineGhost'} size={'xs'}>
             Create new label &quot;{searchValue}&quot;
           </Button>
         </CommandItem>
@@ -203,75 +205,82 @@ type LabelsItem = LabelType & {
   onDelete: () => void;
   isActive: boolean;
   onSelect: () => void;
-  accordionValue: string;
-  setAccordionValue: (newValue: string) => void;
+  editedValue: string;
+  setEditedValue: (newValue: string) => void;
 };
 
-const LabelsListItem = ({ value, label, color, accordionValue, setAccordionValue, onSubmit, onDelete, onSelect, isActive }: LabelsItem) => {
+const LabelsListItem = ({ value, label, color, editedValue, setEditedValue, onSubmit, onDelete, onSelect, isActive }: LabelsItem) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
   const [newLabelName, setNewLabelName] = React.useState<string>(label);
   const [newColorValue, setNewColorValue] = React.useState<string>(color);
   const disabled = label === newLabelName && color === newColorValue;
 
   React.useEffect(() => {
-    if (accordionValue === value) inputRef.current?.focus();
-  }, [accordionValue, value]);
+    if (editedValue === value) inputRef.current?.focus();
+  }, [editedValue, value]);
+
+  const handleEditClick = () => {
+    setEditedValue(value);
+  };
+
+  const handleCancelEdit = () => {
+    setEditedValue('');
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(e);
+    setEditedValue('');
+  };
 
   return (
-    <>
-      <Accordion key={value} type="single" collapsible value={accordionValue} onValueChange={setAccordionValue}>
-        <AccordionItem value={value}>
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-">
-              <Check className={cn('h-4 w-4', isActive ? 'opacity-100' : 'opacity-0')} />
-              <Button size={'micro'} variant={'none'} onClick={onSelect}>
-                <Badge variant="outline" style={badgeStyle(color)}>
-                  {label}
-                </Badge>
-              </Button>
-            </div>
-            <div className="flex items-center gap-3">
-              <AccordionTrigger>Edit</AccordionTrigger>
-              <Button onClick={onDelete} variant="destructive" size="micro">
-                Delete
-              </Button>
-            </div>
+    <div className="py-3 text-sm font-medium transition-all">
+      {editedValue !== value ? (
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <Check className={cn('h-4 w-4', isActive ? 'opacity-100' : 'opacity-0')} />
+            <Button size={'micro'} variant={'none'} onClick={onSelect}>
+              <Badge className="h-8 px-2 rounded-[12px]" variant="outline" style={badgeStyle(color)}>
+                {label}
+              </Badge>
+            </Button>
           </div>
-          <AccordionContent>
-            <form
-              className="flex ml-1 items-end gap-4"
-              onSubmit={(e) => {
-                onSubmit(e);
-                setAccordionValue('');
-              }}
-            >
-              <div className="w-full gap-3 grid">
-                <Label htmlFor="name">Label name</Label>
-                <Input
-                  ref={inputRef}
-                  id="name"
-                  value={newLabelName}
-                  onChange={(e: { target: { value: React.SetStateAction<string> } }) => setNewLabelName(e.target.value)}
-                  className="h-7"
-                />
-              </div>
-              <div className="gap-3 grid">
-                <Label htmlFor="color">Color</Label>
-                <Input
-                  id="color"
-                  type="color"
-                  value={newColorValue}
-                  onChange={(e: { target: { value: React.SetStateAction<string> } }) => setNewColorValue(e.target.value)}
-                  className="h-7 px-2 py-1"
-                />
-              </div>
-              <Button type="submit" disabled={disabled} size="micro">
-                Save
-              </Button>
-            </form>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </>
+          <div className="flex items-center gap-2">
+            <Button onClick={handleEditClick} variant="outlineGhost" size="xs">
+              Edit
+            </Button>
+            <Button onClick={onDelete} variant="destructive" size="xs">
+              Delete
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <form className="flex ml-1 gap-2" onSubmit={handleSubmit}>
+          <Input
+            ref={inputRef}
+            id="name"
+            value={newLabelName}
+            onChange={(e: { target: { value: React.SetStateAction<string> } }) => setNewLabelName(e.target.value)}
+            className="h-8"
+          />
+          <Input
+            id="color"
+            type="color"
+            value={newColorValue}
+            style={{ padding: '0', cursor: 'pointer' }}
+            onChange={(e: { target: { value: React.SetStateAction<string> } }) => setNewColorValue(e.target.value)}
+            className="h-8 w-14"
+          />
+          <div className="gap-1.5 flex">
+            <Button variant="outlineGhost" type="submit" disabled={disabled} size="xs">
+              Save
+            </Button>
+            <Button variant="outlineGhost" onClick={handleCancelEdit} size="xs">
+              Cancel
+            </Button>
+          </div>
+        </form>
+      )}
+    </div>
   );
 };
