89a4f19289d0a8df213c046eebf7bf4c77ebd273
David Hordiienko <neonz96@mail.ru>
2024-04-15 12:39:25 +0300

    fix: indactive -> inactive

--- backend/drizzle/0007_lyrical_mariko_yashida.sql
+++ backend/drizzle/0007_lyrical_mariko_yashida.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "memberships" ADD COLUMN "inactive" boolean DEFAULT false;
+ALTER TABLE "memberships" ADD COLUMN "inactive" boolean DEFAULT false;--> statement-breakpoint
 ALTER TABLE "memberships" ADD COLUMN "muted" boolean DEFAULT false;
