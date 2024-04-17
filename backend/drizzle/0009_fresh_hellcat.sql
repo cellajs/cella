833fe8c963885b8d76640836e749b3a3c1602a10
David Hordiienko <neonz96@mail.ru>
2024-04-17 10:49:03 +0300

    fix: workspace table

--- backend/drizzle/0009_fresh_hellcat.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE "workspaceMembership";--> statement-breakpoint
-ALTER TABLE "memberships" ADD COLUMN "workspace_id" varchar;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "memberships" ADD CONSTRAINT "memberships_workspace_id_workspaces_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "workspaces"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
--- backend/src/db/schema/workspaces.ts
+++ backend/src/db/schema/workspaces.ts
@@ -1,6 +1,5 @@
-import { config } from 'config';
 import { relations } from 'drizzle-orm';
-import { boolean, index, json, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { index, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { nanoid } from '../../lib/nanoid';
 import { usersTable } from './users';
 import { membershipsTable } from './memberships';
@@ -10,25 +9,9 @@ export const workspacesTable = pgTable(
   {
     id: varchar('id').primaryKey().$defaultFn(nanoid),
     name: varchar('name').notNull().unique(),
-    shortName: varchar('short_name').unique(),
     slug: varchar('slug').unique().notNull(),
-    country: varchar('country'),
-    timezone: varchar('timezone'),
-    defaultLanguage: varchar('default_language', {
-      enum: ['en', 'nl'],
-    })
-      .notNull()
-      .default(config.defaultLanguage),
-    languages: json('languages').$type<string[]>().notNull().default([config.defaultLanguage]),
-    notificationEmail: varchar('notification_email'),
-    emailDomains: json('email_domains').$type<string[]>(),
-    brandColor: varchar('brand_color'),
     thumbnailUrl: varchar('thumbnail_url'),
-    logoUrl: varchar('logo_url'),
     bannerUrl: varchar('banner_url'),
-    welcomeText: varchar('welcome_text'),
-    authStrategies: json('auth_strategies').$type<string[]>(),
-    chatSupport: boolean('chat_support').notNull().default(false),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     createdBy: varchar('created_by').references(() => usersTable.id, {
       onDelete: 'set null',
