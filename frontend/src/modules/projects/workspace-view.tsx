2cbd072bb37f64a57447acaf16f5073d699421d0
flipvanhaaren <flip@shareworks.nl>
2024-04-25 11:10:31 +0200

    add workspace view options

--- frontend/src/modules/projects/workspace-view.tsx
+++ frontend/src/modules/projects/workspace-view.tsx
@@ -2,21 +2,50 @@ import { SlidersHorizontal } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import { cn } from '~/lib/utils';
 import { Button } from '~/modules/ui/button';
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from '~/modules/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '~/modules/ui/dropdown-menu';
 import { TooltipButton } from '~/modules/common/tooltip-button';
+import { ToggleGroup, ToggleGroupItem } from '../ui/toggle-group.tsx';
 
-
-interface Props{
+interface Props {
   className?: string;
 }
+
+const viewOptions = [
+  {
+    id: 'type',
+    options: [
+      { value: 'feature', label: 'feature' },
+      { value: 'bug', label: 'bug' },
+      { value: 'chore', label: 'chore' },
+    ],
+  },
+  {
+    id: 'status',
+    options: [
+      { value: '1', label: 'unstarted' },
+      { value: '2', label: 'started' },
+      { value: '4', label: 'delivered' },
+      { value: '5', label: 'reviewed' },
+    ],
+  },
+  {
+    id: 'labels',
+    options: [
+      { value: 'primary', label: 'primary' },
+      { value: 'secondary', label: 'secondary' },
+      { value: 'colors', label: 'colors' },
+    ],
+  },
+];
 
 const WorkspaceView = ({ className = '' }: Props) => {
   const { t } = useTranslation();
 
+  const handleValueChange = (viewOption: string, values: string[]) => {
+    console.log(viewOption, values);
+  };
   return (
-    <DropdownMenu
-
-    >
+    <DropdownMenu>
       <TooltipButton toolTipContent={t('common:view_options')}>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" className={cn('relative flex', className)}>
@@ -25,16 +54,22 @@ const WorkspaceView = ({ className = '' }: Props) => {
           </Button>
         </DropdownMenuTrigger>
       </TooltipButton>
-      <DropdownMenuContent align="end" className="min-w-[220px] pt-2" collisionPadding={16}>
-        <div className="overflow-y-auto relative">
-            <DropdownMenuCheckboxItem
-              className="mx-1"
-              checked={false}
-            >
-              No labels
-            </DropdownMenuCheckboxItem>
-          <div className="sticky bottom-0 h-[8px] bg-gradient-to-t from-popover" />
-        </div>
+      <DropdownMenuContent align="end" className="min-w-[320px] p-2 gap-2 flex flex-col">
+          {viewOptions.map((viewOption) => (
+              <ToggleGroup
+                key={viewOption.id}
+                type="multiple"
+                variant="merged"
+                className={cn('gap-0 w-full', className)}
+                onValueChange={(values) => handleValueChange(viewOption.id, values)}
+              >
+                {viewOption.options.map(({ value, label }) => (
+                  <ToggleGroupItem key={value} size="sm" value={value} className="w-full">
+                    <span className="ml-2 text-sm font-normal">{t(`common:${label}`)}</span>
+                  </ToggleGroupItem>
+                ))}
+              </ToggleGroup>
+          ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
