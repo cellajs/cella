7b71920f8ea1de9908dff09e13e3ab4cb6c69fd9
David Hordiienko <neonz96@mail.ru>
2024-04-29 10:03:02 +0300

    improve: workspace store

--- frontend/src/store/workspace.ts
+++ frontend/src/store/workspace.ts
@@ -3,28 +3,49 @@ import { devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { createJSONStorage } from 'zustand/middleware';
 
+type Column = Record<
+  string,
+  { width: string; minimized: boolean; expandAccepted: boolean; expandIced: boolean; recentLabels: string[]; taskIds: string[] }
+>;
+
+type ViewOptions = {
+  status: ('iced' | 'unstarted' | 'started' | 'finished' | 'delivered' | 'reviewed' | 'accepted')[];
+  type: ('feature' | 'bug' | 'chore')[];
+};
+
 interface WorkspaceState {
-  viewOptions: {
-    status: ('iced' | 'unstarted' | 'started' | 'finished' | 'delivered' | 'reviewed' | 'accepted')[];
-    type: ('feature' | 'bug' | 'chore')[];
-  };
-  displayOption: string;
-  columns: Record<string, { width: string; minimized: boolean; expandAccepted: boolean; expandIced: boolean; recentLabels: string[] }>;
+  viewOptions: ViewOptions;
+  displayOption: 'table' | 'board' | 'tiles';
+  columns: Column[];
 }
 
 export const useWorkspaceStore = create<WorkspaceState>()(
   devtools(
     persist(
-      immer((_) => ({
+      immer((set) => ({
         viewOptions: {
-          status: ['iced', 'unstarted', 'started', 'finished', 'delivered', 'reviewed', 'accepted'],
-          type: ['feature', 'bug', 'chore'],
+          status: [],
+          type: [],
         },
         displayOption: 'table',
-        columns: {
-          'project-id': { width: '200px', minimized: false, expandAccepted: false, expandIced: false, recentLabels: [] },
+        columns: [{}],
+        setColumn: (column: Column) => {
+          set((state) => {
+            state.columns.push(column);
+          });
+        },
+        setDisplayOption: (option: 'table' | 'board' | 'tiles') => {
+          set((state) => {
+            state.displayOption = option;
+          });
+        },
+        changeViewOptions: (newViewOptions: ViewOptions) => {
+          set((state) => {
+            state.viewOptions = newViewOptions;
+          });
         },
       })),
+
       {
         version: 1,
         name: 'workspace',
