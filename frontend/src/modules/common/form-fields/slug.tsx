83aa329ba9821bff531fde5325b4aaa0b2a18734
David Hordiienko <neonz96@mail.ru>
2024-05-01 15:34:31 +0300

    improve: handle inputs

--- frontend/src/modules/common/form-fields/slug.tsx
+++ frontend/src/modules/common/form-fields/slug.tsx
@@ -79,6 +79,7 @@ export const SlugFormField = ({ control, label, previousSlug, description, nameV
       inputClassName={isSlugAvailable && isValidSlug(slug) ? 'ring-2 ring-green-500 focus-visible:ring-2 focus-visible:ring-green-500' : ''}
       onFocus={() => setDeviating(true)}
       label={label}
+      prefix={type.toLowerCase()}
       description={description}
       required
       subComponent={
