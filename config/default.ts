fe32feb10e150f38ababaf27969c7855c84e3726
Roman <nabukhotnyiroman@gmail.com>
2024-05-02 23:46:36 +0300

    +

--- config/default.ts
+++ config/default.ts
@@ -5,7 +5,8 @@ export const config = {
 
   frontendUrl: 'http://localhost:3000',
   backendUrl: 'http://localhost:4000',
-  electricUrl: 'https://cella-electric-dev.onrender.com',
+  // electricUrl: 'https://cella-electric-dev.onrender.com',
+  electricUrl: 'https://electric-z2us.onrender.com',
 
   defaultRedirectPath: '/home',
 
