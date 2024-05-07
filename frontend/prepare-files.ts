c0ec3e4d1a16294f8d44fda7105091bd47cd41e0
Roman <nabukhotnyiroman@gmail.com>
2024-05-07 13:26:15 +0300

    fix: Fix scripts

--- frontend/prepare-files.ts
+++ frontend/prepare-files.ts
@@ -2,7 +2,11 @@ import fs from 'node:fs';
 
 // get .ts files from src/generated and add // @ts-nocheck to the top of each file
 const path = 'src/generated';
-const files = fs.readdirSync(path);
+const files = fs.readdirSync(path, {
+  recursive: true,
+  encoding: 'utf8',
+});
+
 for (const file of files) {
   if (file.endsWith('.ts')) {
     const filePath = `${path}/${file}`;
@@ -10,3 +14,5 @@ for (const file of files) {
     fs.writeFileSync(filePath, `// @ts-nocheck\n${content}`);
   }
 }
+
+console.log('Files prepared');
