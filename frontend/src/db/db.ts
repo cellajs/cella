6248bce41e48b003320590277a6a99d08e75b7c5
Roman <nabukhotnyiroman@gmail.com>
2024-05-07 11:23:39 +0300

    fix: Fix scripts

--- frontend/src/db/db.ts
+++ frontend/src/db/db.ts
@@ -3,11 +3,7 @@ import postgres from 'postgres';
 
 import { config } from 'config';
 
-const client = postgres(
-  config.mode === 'production'
-    ? 'postgresql://prisma:proxy_password@3.75.158.163:65432/postgres'
-    : 'postgres://postgres:proxy_password@0.0.0.0:65431/electric',
-);
+const client = postgres('postgres://postgres:proxy_password@0.0.0.0:65431/electric');
 
 export const db = drizzle(client, {
   logger: config.debug,
