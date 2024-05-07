e347e30e250cf63acfa5e5dfd23718d24e4759af
Roman <nabukhotnyiroman@gmail.com>
2024-05-07 13:33:24 +0300

    fix: Fix electricUrl

--- config/production.ts
+++ config/production.ts
@@ -6,5 +6,6 @@ export default {
   frontendUrl: 'https://cellajs.com',
   backendUrl: 'https://cellajs.com/api/v1',
   tusUrl: 'https://cellajs.com/upload',
-  electricUrl: 'https://cellajs.com/electric',
+  // electricUrl: 'https://cellajs.com/electric',
+  electricUrl: 'https://electric-z2us.onrender.com',
 } satisfies Config;
