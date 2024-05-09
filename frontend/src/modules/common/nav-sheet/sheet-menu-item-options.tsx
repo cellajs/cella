50d0d66b6ca82b538500069092b8b14fdde24fcd
David Hordiienko <neonz96@mail.ru>
2024-05-09 16:26:51 +0300

    add: Pragmatic drag and drop

--- frontend/src/modules/common/nav-sheet/sheet-menu-item-options.tsx
+++ frontend/src/modules/common/nav-sheet/sheet-menu-item-options.tsx
@@ -1,6 +1,5 @@
-import { useSortable } from '@dnd-kit/sortable';
 import { Archive, BellOff, GripVertical, ArchiveRestore, Bell } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
 import { updateMembership } from '~/api/memberships';
@@ -9,23 +8,51 @@ import { Button } from '~/modules/ui/button';
 import { useNavigationStore } from '~/store/navigation';
 import { useUserStore } from '~/store/user';
 import type { Page } from '~/types';
-import { CSS } from '@dnd-kit/utilities';
+import { draggable, dropTargetForElements, monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
+import { attachClosestEdge, type Edge, extractClosestEdge } from '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge';
+import { DropIndicator } from '@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box';
+import { autoScrollForElements } from '@atlaskit/pragmatic-drag-and-drop-auto-scroll/element';
 
 interface SheetMenuItemProps {
   item: Page;
+  sectionName: 'organizations' | 'projects' | 'workspaces';
 }
 
-export const SheetMenuItemOptions = ({ item }: SheetMenuItemProps) => {
+const itemKey = Symbol('item');
+
+type ItemData = {
+  [itemKey]: true;
+  item: Page;
+  index: number;
+};
+
+const arrayMove = (array: string[], startIndex: number, endIndex: number) => {
+  const newArray = [...array];
+  const [removedElement] = newArray.splice(startIndex, 1);
+  newArray.splice(endIndex, 0, removedElement);
+  return newArray;
+};
+
+const getItemData = (item: Page, items: string[]): ItemData => {
+  return { [itemKey]: true, item: item, index: items.findIndex((el) => el === item.id) };
+};
+
+function isItemData(data: Record<string | symbol, unknown>): data is ItemData {
+  return data[itemKey] === true;
+}
+
+export const SheetMenuItemOptions = ({ item, sectionName }: SheetMenuItemProps) => {
+  const dragRef = useRef(null);
   const { t } = useTranslation();
+  const [dragging, setDragging] = useState(false);
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+  const [closestEdge, setClosestEdge] = useState<Edge | null>(null);
   const [isItemArchived, setItemArchived] = useState(item.archived);
   const [isItemMuted, setItemMuted] = useState(item.muted);
   const user = useUserStore((state) => state.user);
   const archiveStateToggle = useNavigationStore((state) => state.archiveStateToggle);
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: item.id });
-  const style = {
-    transition,
-    transform: CSS.Transform.toString(transform),
-  };
+  const { activeItemsOrder, setActiveItemsOrder } = useNavigationStore();
 
   const itemArchiveStateHandle = () => {
     const itemArchiveStatus = !isItemArchived;
@@ -54,11 +81,82 @@ export const SheetMenuItemOptions = ({ item }: SheetMenuItemProps) => {
       });
   };
 
+  useEffect(() => {
+    const element = dragRef.current;
+    const data = getItemData(item, activeItemsOrder[sectionName]);
+    if (!element) return;
+
+    combine(
+      draggable({
+        element,
+        getInitialData: () => data,
+        onDragStart: () => setDragging(true),
+        onDrop: () => setDragging(false),
+      }),
+      dropTargetForElements({
+        element,
+        canDrop({ source }) {
+          return isItemData(source.data) && source.data.item.id !== item.id && source.data.item.type === item.type;
+        },
+        getData({ input }) {
+          return attachClosestEdge(data, {
+            element,
+            input,
+            allowedEdges: ['top', 'bottom'],
+          });
+        },
+        onDrag({ self, source }) {
+          if (source.element === element) {
+            setClosestEdge(null);
+            return;
+          }
+          const closestEdge = extractClosestEdge(self.data);
+          setClosestEdge(closestEdge);
+        },
+        onDragLeave() {
+          setClosestEdge(null);
+          setIsDraggedOver(false);
+        },
+        onDrop() {
+          setClosestEdge(null);
+          setIsDraggedOver(false);
+        },
+        onDragEnter: () => setIsDraggedOver(true),
+      }),
+      autoScrollForElements({
+        element,
+        getConfiguration: () => ({
+          maxScrollSpeed: 'standard',
+        }),
+        getAllowedAxis: () => 'vertical',
+      }),
+    );
+    return monitorForElements({
+      canMonitor({ source }) {
+        return isItemData(source.data) && source.data.item.id === item.id;
+      },
+      onDrop({ location, source }) {
+        const target = location.current.dropTargets[0];
+        const sourceData = source.data;
+        if (!target || !isItemData(sourceData) || !isItemData(target.data)) return;
+
+        const targetData = target.data;
+        const indexOfTarget = activeItemsOrder[sectionName].findIndex((item) => item === targetData.item.id);
+        if (indexOfTarget < 0) return;
+
+        const newItemOrder = arrayMove(activeItemsOrder[sectionName], Number(sourceData.index), indexOfTarget);
+        setActiveItemsOrder(sectionName, newItemOrder);
+      },
+    });
+  }, [item, activeItemsOrder[sectionName]]);
+
   return (
     <div
-      style={style}
-      className={`group mb-1 flex sm:max-w-[18rem] h-14 w-full cursor-pointer items-start justify-start rounded p-0 focus:outline-none
-      ${!isItemArchived ? 'ring-1' : ''} ring-inset ring-muted/25 focus:ring-foreground hover:bg-accent/50 hover:text-accent-foreground`}
+      ref={dragRef}
+      style={{ opacity: `${dragging ? 0.3 : 1}` }}
+      className={`group mb-0.5 flex relative sm:max-w-[18rem] h-14 w-full cursor-pointer items-start justify-start rounded p-0 focus:outline-none
+      ring-inset ring-muted/25 focus:ring-foreground hover:bg-accent/50 hover:text-accent-foreground 
+      ${!isItemArchived ? 'ring-1' : ''} ${isDraggedOver ? 'bg-accent/80' : ''} `}
     >
       <AvatarWrap className="m-2" type={item.type} id={item.id} name={item.name} url={item.thumbnailUrl} />
       <div className="truncate grow p-2 pl-2 text-left">
@@ -97,9 +195,10 @@ export const SheetMenuItemOptions = ({ item }: SheetMenuItemProps) => {
             )}
           </Button>
         </div>
-      </div>
+      </div>{' '}
+      {closestEdge && <DropIndicator edge={closestEdge} gap="2px" />}
       {!isItemArchived && (
-        <div ref={setNodeRef} {...listeners} {...attributes} className="p-2 cursor-grab">
+        <div className="p-2 cursor-grab">
           <GripVertical size={16} className="mt-3 mr-1 opacity-50 transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
         </div>
       )}
