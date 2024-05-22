1c6ad7be438eb542e47262c91d7175e5f1ca4d81
yossydev <codeyy021@gmail.com>
2024-05-22 23:40:42 +0900

    wip

--- blog/app/islands/Nav.tsx
+++ blog/app/islands/Nav.tsx
@@ -1,5 +1,5 @@
-import MarketingPage from 'frontend/src/modules/marketing/page';
-import type { FC, PropsWithChildren } from 'react';
+import MarketingPage from "~/modules/marketing/page";
+import type { FC, PropsWithChildren } from "react";
 
 type Props = PropsWithChildren;
 
