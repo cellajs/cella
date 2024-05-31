6362b56f2ee6cfba4f8f11d9fab1502ad744ae27
David Hordiienko <neonz96@mail.ru>
2024-05-31 15:01:39 +0300

    improve: sheet menu

--- frontend/src/modules/common/nav-sheet/sheet-menu-items.tsx
+++ frontend/src/modules/common/nav-sheet/sheet-menu-items.tsx
@@ -42,13 +42,20 @@ export const SheetMenuItem = ({ item, type, className, submenu, searchResults }:
       params={{ idOrSlug: type === 'PROJECT' ? (item.workspaceId as string) : item.slug }}
       activeProps={{ className: 'bg-accent/50 text-accent-foreground ring-primary/50 text-primary focus:ring-primary' }}
     >
-
-      <AvatarWrap className={`${submenu ? 'my-2 mx-3 h-8 w-8 text-xs' : 'm-2'} z-[1] items-center`} type={type} id={item.id} name={item.name} url={item.thumbnailUrl} />
-      <div className='truncate py-2 flex flex-col justify-center text-left'>
+      <AvatarWrap
+        className={`${submenu ? 'my-2 mx-3 h-8 w-8 text-xs' : 'm-2'} z-[1] items-center`}
+        type={type}
+        id={item.id}
+        name={item.name}
+        url={item.thumbnailUrl}
+      />
+      <div className="truncate py-2 flex flex-col justify-center text-left">
         <div className={`truncate ${submenu ? 'max-sm:pt-[6px] text-sm' : 'max-sm:pt-[10px] text-base'} leading-5`}>{item.name}</div>
         <div className={`max-sm:hidden text-muted-foreground ${submenu ? 'text-xs' : 'text-sm'} font-light`}>
           {searchResults && <span className="inline transition-all duration-500 ease-in-out group-hover:hidden ">{t(type.toLowerCase())}</span>}
-          {item.role && <span className="hidden transition-all duration-500 ease-in-out group-hover:inline ">{UserRole[item.role]}</span>}
+          <span className="hidden transition-all duration-500 ease-in-out group-hover:inline ">
+            {type === 'WORKSPACE' ? `${t('common:projects')} amount: ${item.submenu?.items.length || 0}` : item.role ? UserRole[item.role] : ''}
+          </span>
         </div>
       </div>
     </Link>
