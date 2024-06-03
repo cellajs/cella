4842b729c96c1533f88076a7e3a6d0abc0af3e01
David Hordiienko <neonz96@mail.ru>
2024-06-03 13:59:01 +0300

    improve: entity

--- backend/src/modules/general/helpers/check-slug.ts
+++ backend/src/modules/general/helpers/check-slug.ts
@@ -1,11 +1,11 @@
 import { config } from 'config';
-import { resolveResourceByIdOrSlug } from '../../../middlewares/guard/is-allowed-to';
+import { resolveEntity } from '../../../lib/entity';
 
 // * Check if a slug is available in any of the entities
 export const checkSlugAvailable = async (slug: string) => {
   const entities = config.entityTypes;
 
-  const promises = entities.map((entity) => resolveResourceByIdOrSlug(entity, slug));
+  const promises = entities.map((entity) => resolveEntity(entity, slug));
   const results = await Promise.all(promises);
 
   // Check if any result is found, if so, slug is not available
