7da99262d58a4ac452f285df0928d4c25106f67a
gino1991 <ginopresilli@gmail.com>
2024-06-05 15:51:59 +0200

    Refresh SQL migrations

--- backend/drizzle/0002_fat_pretty_boy.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "memberships" ADD COLUMN "entity" varchar DEFAULT 'membership' NOT NULL;--> statement-breakpoint
-ALTER TABLE "oauth_accounts" ADD COLUMN "entity" varchar DEFAULT 'oauthAccount' NOT NULL;--> statement-breakpoint
-ALTER TABLE "organizations" ADD COLUMN "entity" varchar DEFAULT 'organization' NOT NULL;--> statement-breakpoint
-ALTER TABLE "projects" ADD COLUMN "entity" varchar DEFAULT 'project' NOT NULL;--> statement-breakpoint
-ALTER TABLE "requests" ADD COLUMN "entity" varchar DEFAULT 'request' NOT NULL;--> statement-breakpoint
-ALTER TABLE "sessions" ADD COLUMN "entity" varchar DEFAULT 'session' NOT NULL;--> statement-breakpoint
-ALTER TABLE "tokens" ADD COLUMN "entity" varchar DEFAULT 'token' NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "entity" varchar DEFAULT 'user' NOT NULL;--> statement-breakpoint
-ALTER TABLE "workspaces" ADD COLUMN "entity" varchar DEFAULT 'workspace' NOT NULL;
--- backend/drizzle/0004_easy_mesmero.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "projects" DROP CONSTRAINT "projects_workspace_id_workspaces_id_fk";
---> statement-breakpoint
-ALTER TABLE "projects" DROP COLUMN IF EXISTS "workspace_id";
