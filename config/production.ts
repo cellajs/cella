3011181a356cba4b73eb05852f220209e8a36ff7
flipvanhaaren <flip@shareworks.nl>
2024-06-13 15:22:34 +0200

    try without proxy

--- config/production.ts
+++ config/production.ts
@@ -5,9 +5,9 @@ export default {
   maintenance: false,
 
   frontendUrl: 'https://cellajs.com',
-  backendUrl: 'https://cellajs.com/api/v1',
-  tusUrl: 'https://cellajs.com/upload/v1',
-  electricUrl: 'https://cellajs.com/electric/v1',
+  backendUrl: 'https://api.cellajs.com',
+  tusUrl: 'https://tus.cellajs.com',
+  electricUrl: 'https://electric-sync.cellajs.com',
 
   googleMapsKey: 'AIzaSyAGx1ZAPoNIu8tUWD4F0D2B3XwAOaSMMH8',
 } satisfies Config;
