45779bd1f8e2301ec8f822d23b9238bf83073def
David Hordiienko <neonz96@mail.ru>
2024-07-12 11:45:53 +0300

    refactor: select-labels

--- frontend/src/modules/projects/task/task-selectors/select-labels.tsx
+++ frontend/src/modules/projects/task/task-selectors/select-labels.tsx
@@ -55,6 +55,7 @@ const SetLabels = ({ value, projectId, organizationId, taskUpdateCallback, creat
   };
 
   const showedLabels = useMemo(() => {
+    if (isSearching) return labels.filter((l) => l.name.includes(searchValue));
     if (selectedLabels.length > 8) return selectedLabels;
     const nonSelectedLabels = labels.filter((label) => !selectedLabels.some((selected) => selected.id === label.id));
     // save to recent labels all labels that used in past 3 days
@@ -63,11 +64,6 @@ const SetLabels = ({ value, projectId, organizationId, taskUpdateCallback, creat
     });
     return [...selectedLabels, ...nonSelectedLabels].slice(0, 8);
   }, [selectedLabels, labels]);
-
-  const searchedLabels: Label[] = useMemo(() => {
-    if (isSearching) return labels.filter((l) => l.name.includes(searchValue));
-    return [];
-  }, [searchValue, labels]);
 
   const createLabel = (newLabel: Label) => {
     if (!Electric) return toast.error(t('common:local_db_inoperable'));
@@ -180,58 +176,31 @@ const SetLabels = ({ value, projectId, organizationId, taskUpdateCallback, creat
               <Loader2 className="text-muted-foreground h-6 w-6 mx-auto mt-2 animate-spin" />
             </CommandLoading>
           )}
-          {!isSearching ? (
-            <>
-              {labels.length === 0 && (
-                <CommandEmpty className="text-muted-foreground text-sm flex items-center justify-center px-3 py-2">
-                  {t('common:no_resource_yet', { resource: t('common:labels').toLowerCase() })}
-                </CommandEmpty>
-              )}
-              {showedLabels.map((label, index) => (
-                <CommandItem
-                  key={label.id}
-                  value={label.name}
-                  onSelect={(value) => {
-                    handleSelectClick(value);
-                  }}
-                  className="group rounded-md flex justify-between items-center w-full leading-normal"
-                >
-                  <div className="flex items-center gap-2">
-                    {isSearching ? <Dot className="rounded-md" size={16} style={badgeStyle(label.color)} strokeWidth={8} /> : <History size={16} />}
-                    <span>{label.name}</span>
-                  </div>
-                  <div className="flex items-center">
-                    {selectedLabels.some((l) => l.id === label.id) && <Check size={16} className="text-success" />}
-                    {!isSearching && <span className="max-xs:hidden text-xs opacity-50 ml-3 mr-1">{index + 1}</span>}
-                  </div>
-                </CommandItem>
-              ))}
-            </>
-          ) : searchedLabels.length > 0 ? (
-            <>
-              {searchedLabels.map((label, index) => (
-                <CommandItem
-                  key={label.id}
-                  value={label.name}
-                  onSelect={(value) => {
-                    handleSelectClick(value);
-                  }}
-                  className="group rounded-md flex justify-between items-center w-full leading-normal"
-                >
-                  <div className="flex items-center gap-2">
-                    {isSearching ? <Dot className="rounded-md" size={16} style={badgeStyle(label.color)} strokeWidth={8} /> : <History size={16} />}
-                    <span>{label.name}</span>
-                  </div>
-                  <div className="flex items-center">
-                    {selectedLabels.some((l) => l.id === label.id) && <Check size={16} className="text-success" />}
-                    {!isSearching && <span className="max-xs:hidden text-xs opacity-50 ml-3 mr-1">{index + 1}</span>}
-                  </div>
-                </CommandItem>
-              ))}
-            </>
-          ) : (
-            <CommandItemCreate onSelect={() => handleCreateClick(searchValue)} {...{ searchValue, labels }} />
+          {isSearching && !!showedLabels.length && <CommandItemCreate onSelect={() => handleCreateClick(searchValue)} {...{ searchValue, labels }} />}
+          {labels.length === 0 && (
+            <CommandEmpty className="text-muted-foreground text-sm flex items-center justify-center px-3 py-2">
+              {t('common:no_resource_yet', { resource: t('common:labels').toLowerCase() })}
+            </CommandEmpty>
           )}
+          {showedLabels.map((label, index) => (
+            <CommandItem
+              key={label.id}
+              value={label.name}
+              onSelect={(value) => {
+                handleSelectClick(value);
+              }}
+              className="group rounded-md flex justify-between items-center w-full leading-normal"
+            >
+              <div className="flex items-center gap-2">
+                {isSearching ? <Dot className="rounded-md" size={16} style={badgeStyle(label.color)} strokeWidth={8} /> : <History size={16} />}
+                <span>{label.name}</span>
+              </div>
+              <div className="flex items-center">
+                {selectedLabels.some((l) => l.id === label.id) && <Check size={16} className="text-success" />}
+                {!isSearching && <span className="max-xs:hidden text-xs opacity-50 ml-3 mr-1">{index + 1}</span>}
+              </div>
+            </CommandItem>
+          ))}
         </CommandGroup>
       </CommandList>
     </Command>
