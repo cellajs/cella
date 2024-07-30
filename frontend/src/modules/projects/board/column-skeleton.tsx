5a407395f47a8c0c119f571de96444262cd3d203
flipvanhaaren <flip@shareworks.nl>
2024-07-30 11:19:57 +0200

    update tanstack

--- frontend/src/modules/projects/board/column-skeleton.tsx
+++ frontend/src/modules/projects/board/column-skeleton.tsx
@@ -11,7 +11,7 @@ export const ColumnSkeleton = ({ rowHeight = 88, rowCount = 8 }: Props) => {
   const renderRowHeight = rowHeight - 8;
   const { hasStarted } = useMounted();
   return (
-    <div className={`w-full  overflow-auto  flex flex-col transition-opacity duration-300 ${hasStarted ? 'opacity-100' : 'opacity-0'}`}>
+    <div className={`w-full overflow-auto flex flex-col transition-opacity duration-300 ${hasStarted ? 'opacity-100' : 'opacity-0'}`}>
       <div className="flex justify-start w-full h-9 rounded-none gap-1 border-b border-b-green-500/10 ring-inset bg-green-500/5 -mt-[.07rem]" />
       {Array.from({ length: rowCount }).map((_, index) => (
         // biome-ignore lint/suspicious/noArrayIndexKey: skeleton is not undergoing mutations
