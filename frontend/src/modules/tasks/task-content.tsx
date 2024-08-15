c451adb383794848904e813cf6b0ec427afcedca
David Hordiienko <neonz96@mail.ru>
2024-08-15 17:01:39 +0300

    fix: css style of task card before&after isExpanded

--- frontend/src/modules/tasks/task-content.tsx
+++ frontend/src/modules/tasks/task-content.tsx
@@ -24,7 +24,7 @@ const TaskContent = ({ task, mode, isSheet, isExpanded }: Props) => {
       {!isExpanded ? (
         <div className="mt-1">
           {/* biome-ignore lint/security/noDangerouslySetInnerHtml: to avoid using TaskBlockNote for not editing */}
-          <div dangerouslySetInnerHTML={{ __html: task.summary as string }} className="inline font-light task-summary" />
+          <div dangerouslySetInnerHTML={{ __html: task.summary as string }} className="task-summary" />
           <div className="inline-flex gap-1 items-center opacity-80 group-hover/task:opacity-100 group-[.is-focused]/task:opacity-100 px-2">
             {(task.summary !== task.description || task.subTasks.length > 0) && (
               <Button variant="link" size="micro" onClick={() => dispatchCustomEvent('toggleCard', task.id)} className="inline-flex py-0 h-5">
