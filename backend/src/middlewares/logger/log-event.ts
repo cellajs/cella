80933737799f94a53bf774e976ed200410cfa106
flipvanhaaren <flip@shareworks.nl>
2024-09-03 19:18:53 +0200

    disable logtail if no logtail token is given

--- backend/src/middlewares/logger/log-event.ts
+++ backend/src/middlewares/logger/log-event.ts
@@ -2,14 +2,14 @@ import { Logtail } from '@logtail/node';
 import type { EventData, Severity } from '#/lib/errors';
 import { env } from '../../../env';
 
-export const logtail = new Logtail(env.LOGTAIL_TOKEN || 'test', {});
+export const logtail = env.LOGTAIL_TOKEN ? new Logtail(env.LOGTAIL_TOKEN, {}) : undefined;
 
 export const logEvent = (message: string, eventData?: EventData, severity: Severity = 'info') => {
   if (eventData) {
     console[severity](message, eventData);
-    logtail[severity](message, undefined, eventData);
+    if (logtail) logtail[severity](message, undefined, eventData);
   } else {
     console[severity](message);
-    logtail[severity](message);
+    if (logtail) logtail[severity](message);
   }
 };
