2f8028c2d9dca994d944d950417532950da1eba3
Roman <nabukhotnyiroman@gmail.com>
2024-09-10 12:05:26 +0300

    fix: FIx drizzle config

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 import { env } from './env';
 
+const extendConfig = env.PGLITE ? { driver: 'pglite'} : {};
+
 export default defineConfig({
   schema: './src/db/schema/*',
   out: './drizzle',
   dialect: 'postgresql',
-  driver: env.PGLITE ? 'pglite' : undefined,
+  ...extendConfig,
   dbCredentials: {
     url: env.PGLITE ? './.db' : env.DATABASE_URL,
   },
