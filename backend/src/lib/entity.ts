4ec4f5d840cce7f56a2185a7474d23a713c9d6b8
Roman <nabukhotnyiroman@gmail.com>
2024-09-10 15:48:07 +0300

    refactor: Refactor resolveEntity

--- backend/src/lib/entity.ts
+++ backend/src/lib/entity.ts
@@ -1,33 +1,14 @@
 import { eq, inArray, or } from 'drizzle-orm';
 import { db } from '#/db/db';
 import { entityTables } from '#/entity-config';
-import type { ContextEntity, Entity } from '#/types/common';
-
-/**
- * Resolves context entity(those who have memberships) based on ID or Slug and sets the context accordingly.
- * @param entityType - The type of the context entity(with membership).
- * @param idOrSlug - The unique identifier (ID or Slug) of the entity.
- */
-export const resolveContextEntity = async (entityType: ContextEntity, idOrSlug: string) => {
-  const table = entityTables[entityType];
-
-  // Return early if table is not available
-  if (!table) throw new Error(`Invalid entity: ${entityType}`);
-
-  const [entity] = await db
-    .select()
-    .from(table)
-    .where(or(eq(table.id, idOrSlug), eq(table.slug, idOrSlug)));
-
-  return entity;
-};
+import type { Entity } from '#/types/common';
 
 /**
  * Resolves entity based on ID or Slug and sets the context accordingly.
  * @param entityType - The type of the entity.
  * @param idOrSlug - The unique identifier (ID or Slug) of the entity.
  */
-export const resolveEntity = async (entityType: Entity, idOrSlug: string) => {
+export const resolveEntity = async <T extends Entity>(entityType: T, idOrSlug: string): Promise<(typeof entityTables)[T]['$inferSelect']> => {
   const table = entityTables[entityType];
 
   // Return early if table is not available
@@ -38,7 +19,7 @@ export const resolveEntity = async (entityType: Entity, idOrSlug: string) => {
     .from(table)
     .where(or(eq(table.id, idOrSlug), eq(table.slug, idOrSlug)));
 
-  return entity;
+  return entity as (typeof entityTables)[T]['$inferSelect'];
 };
 
 /**
