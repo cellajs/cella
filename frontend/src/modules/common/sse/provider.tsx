a54fa49344274b44e2c4b3a57b0e374722bf3052
David Hordiienko <shiraz0david@gmail.com>
2024-09-13 12:48:00 +0300

    restart SSE when connection is closed and also do a new get me/menu.

--- frontend/src/modules/common/sse/provider.tsx
+++ frontend/src/modules/common/sse/provider.tsx
@@ -1,5 +1,6 @@
 import { config } from 'config';
 import { type FC, createContext, createElement, useEffect, useState } from 'react';
+import { getAndSetMe, getAndSetMenu } from '~/modules/users/helpers';
 
 export const SSEContext = createContext<EventSource | null>(null);
 
@@ -10,15 +11,37 @@ type Props = React.PropsWithChildren;
 export const SSEProvider: FC<Props> = ({ children }) => {
   const [source, setSource] = useState<EventSource | null>(null);
 
-  useEffect(() => {
+  const createResource = () => {
     const eventSource = new EventSource(`${config.backendUrl}/sse`, {
       withCredentials: true,
     });
 
+    eventSource.onerror = () => {
+      // Handle the error and try to reconnect
+      console.error('SSE connection error. Reconnecting...');
+      eventSource.close();
+      reconnect();
+    };
+
+    return eventSource;
+  };
+  const reconnect = async () => {
+    if (source) source.close(); // Delete old source if it exists
+    const newSource = createResource();
+    setSource(newSource);
+    await Promise.all([getAndSetMe(), getAndSetMenu()]);
+  };
+
+  useEffect(() => {
+    const eventSource = createResource();
     setSource(eventSource);
 
+    const handleVisibilityChange = async () => document.visibilityState === 'visible' && reconnect();
+
+    window.addEventListener('visibilitychange', handleVisibilityChange);
     return () => {
       eventSource.close();
+      window.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, []);
 
