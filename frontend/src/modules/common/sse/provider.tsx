8dc603f6315f0abd1508cbce3443da726eae96a6
David Hordiienko <shiraz0david@gmail.com>
2024-09-13 15:50:22 +0300

    improve: sse reconnect logic

--- frontend/src/modules/common/sse/provider.tsx
+++ frontend/src/modules/common/sse/provider.tsx
@@ -1,6 +1,6 @@
-import { config } from 'config';
 import { type FC, createContext, createElement, useEffect, useState } from 'react';
 import { getAndSetMe, getAndSetMenu } from '~/modules/users/helpers';
+import { createResource } from './helpers';
 
 export const SSEContext = createContext<EventSource | null>(null);
 
@@ -10,42 +10,45 @@ type Props = React.PropsWithChildren;
 
 export const SSEProvider: FC<Props> = ({ children }) => {
   const [source, setSource] = useState<EventSource | null>(null);
+  const [retryCount, setRetryCount] = useState(0);
 
-  const createResource = () => {
-    const eventSource = new EventSource(`${config.backendUrl}/sse`, {
-      withCredentials: true,
-    });
-
-    eventSource.onerror = () => {
-      // Handle the error and try to reconnect
-      console.error('SSE connection error. Reconnecting...');
-      eventSource.close();
+  // Tries up to 3 times, then pauses for 1 minute if unsuccessful
+  const handleReconnect = async () => {
+    if (retryCount < 3) {
       reconnect();
-    };
-
-    return eventSource;
+      setRetryCount(retryCount + 1);
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 60000)); // 1m wait
+      setRetryCount(0);
+      reconnect(); // Retry after the pause
+    }
   };
+
+  // Closes old source, creates new
   const reconnect = async () => {
-    if (source) source.close(); // Delete old source if it exists
-    const newSource = createResource();
+    if (source) source.close(); // Close old if it exists
+    const newSource = createResource(handleReconnect);
     setSource(newSource);
-    await Promise.all([getAndSetMe(), getAndSetMenu()]);
+    await Promise.all([getAndSetMe(), getAndSetMenu()]); // Refetch data some sse events might have been skipped
   };
 
-  useEffect(() => {
-    const eventSource = createResource();
-    setSource(eventSource);
-    return () => eventSource.close();
-  }, []);
-
+  // Effect to handle reconnecting when the tab becomes visible again
   useEffect(() => {
     const handleVisibilityChange = async () => {
-      if (document.visibilityState === 'visible' && source && source.readyState === EventSource.CLOSED) await reconnect();
+      // Reconnect if the page becomes visible and the SSE connection is closed
+      if (document.visibilityState !== 'visible' || source?.readyState !== EventSource.CLOSED) return;
+      await reconnect();
     };
 
     window.addEventListener('visibilitychange', handleVisibilityChange);
     return () => window.removeEventListener('visibilitychange', handleVisibilityChange);
   }, [source]);
+
+  useEffect(() => {
+    const eventSource = createResource(handleReconnect);
+    setSource(eventSource);
+    return () => eventSource.close();
+  }, []);
 
   return createElement(
     SSEContext.Provider,
