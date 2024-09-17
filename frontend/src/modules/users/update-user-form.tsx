423ec4f44181181c3333a9754c26b6532a30fd6d
David Hordiienko <shiraz0david@gmail.com>
2024-09-17 14:15:54 +0300

    add: outline-glow-button css

--- frontend/src/modules/users/update-user-form.tsx
+++ frontend/src/modules/users/update-user-form.tsx
@@ -101,9 +101,9 @@ const UpdateUserForm = ({ user, callback, sheet: isSheet, hiddenFields, children
           showToast(t('common:success.you_updated'), 'success');
         } else showToast(t('common:success.updated_user'), 'success');
         form.reset(updatedUser);
-        callback?.(updatedUser);
         if (isSheet) sheet.remove('update-user');
         nextStep?.();
+        callback?.(updatedUser);
       },
     });
   };
