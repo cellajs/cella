f03074ecf1467a35a32bbc0f41252fad8eb5b474
David Hordiienko <shiraz0david@gmail.com>
2024-09-20 17:36:10 +0300

    fix:  columns view styles

--- frontend/src/modules/common/data-table/columns-view.tsx
+++ frontend/src/modules/common/data-table/columns-view.tsx
@@ -39,7 +39,9 @@ const ColumnsView = <TData,>({ columns, setColumns, className = '' }: Props<TDat
       <TooltipButton className={className} toolTipContent={t('common:columns_view')}>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" className="relative flex">
-            {filteredColumns.some((column) => !column.visible) && <Badge className="absolute -right-1 -top-1 flex h-2 w-2 justify-center p-0" />}
+            {filteredColumns.some((column) => !column.visible) && (
+              <Badge className="absolute -right-1 -top-1 flex h-2 w-2 justify-center p-0 z-[100]" />
+            )}
             <SlidersHorizontal className="h-4 w-4" />
             <span className="ml-1 max-xl:hidden">{t('common:view')}</span>
           </Button>
