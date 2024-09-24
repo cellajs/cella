5d9cf7dc7f438b5fa826a36a535cc10b67295131
David Hordiienko <shiraz0david@gmail.com>
2024-09-24 12:59:25 +0300

    refactor: seed

--- backend/scripts/seed.ts
+++ backend/scripts/seed.ts
@@ -1,16 +1,10 @@
 import { execSync } from 'node:child_process';
-import { argv } from 'node:process';
 import { config } from 'config';
-
-const args = argv.slice(2);
-const addImagesFlag = args.includes('--addImages');
 
 for (const cmd of config.seedScripts) {
   try {
-    // Conditionally append the --addImages flag if applicable
-    const command = cmd.includes('seed:organizations') && addImagesFlag ? `${cmd} --addImages` : cmd;
     // Execute the command
-    execSync(command, { stdio: 'inherit' });
+    execSync(cmd, { stdio: 'inherit' });
   } catch (error) {
     console.error(`Error executing command: ${cmd}`, error);
     process.exit(1);
