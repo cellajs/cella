a79179b3051e569080c97c2466ab6f1938ff261b
David Hordiienko <shiraz0david@gmail.com>
2024-09-26 10:34:40 +0300

    refactor: board search query update

--- frontend/src/modules/projects/board/header/board-search.tsx
+++ frontend/src/modules/projects/board/header/board-search.tsx
@@ -1,43 +1,29 @@
-import { useNavigate } from '@tanstack/react-router';
 import { Search, XCircle } from 'lucide-react';
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDebounce } from '~/hooks/use-debounce';
+import useSaveInSearchParams from '~/hooks/use-save-in-search-params';
 import { TableFilterBarContext } from '~/modules/common/data-table/table-filter-bar';
 import { Input } from '~/modules/ui/input';
 import { useWorkspaceStore } from '~/store/workspace';
 
 const BoardSearch = ({ toggleFocus }: { toggleFocus: () => void }) => {
-  const navigate = useNavigate();
   const { t } = useTranslation();
   // Reference with `useRef` to persist the same ref object during re-renders
   const inputRef = useRef<HTMLInputElement>(null);
   const { isFilterActive } = useContext(TableFilterBarContext);
   const { selectedTasks, setSelectedTasks, searchQuery, setSearchQuery } = useWorkspaceStore();
-  const debouncedSearchQuery = useDebounce(searchQuery, 300);
 
   const handleClick = () => {
     inputRef.current?.focus();
   };
 
-  // TODO refactor this
-  useEffect(() => {
-    const q = debouncedSearchQuery;
-    if (!q.length) {
-      navigate({
-        to: '.',
-        search: (prev) => {
-          const { q, ...rest } = prev;
-          return rest;
-        },
-      });
-    } else {
-      navigate({
-        to: '.',
-        search: (prev) => ({ ...prev, q }),
-      });
-    }
-  }, [debouncedSearchQuery]);
+  const filters = useMemo(
+    () => ({
+      q: searchQuery,
+    }),
+    [searchQuery],
+  );
+  useSaveInSearchParams(filters);
 
   // Focus input when filter button clicked(mobile)
   useEffect(() => {
