1265ee170041cf892d66b48e58401be7e6017ea4
gino1991 <ginopresilli@gmail.com>
2024-09-27 13:21:59 +0200

    cli/create-cella > Fix error when folder is already git initialized

--- cli/create-cella/src/create.js
+++ cli/create-cella/src/create.js
@@ -1,4 +1,6 @@
 import { mkdir } from 'node:fs/promises';
+import { existsSync } from 'node:fs'
+import { join } from 'node:path';
 import colors from 'picocolors';
 import { downloadTemplate } from "giget";
 import yoctoSpinner from 'yocto-spinner';
@@ -81,24 +83,30 @@ export async function create({
       const gitSpinner = yoctoSpinner({
         text: 'initializing git repository',
       }).start()
-      
-      try {
-        await runGitCommand({ targetFolder, command: 'init' })
-        await runGitCommand({ targetFolder, command: 'add .' })
-        await runGitCommand({ targetFolder, command: 'commit -m "Initial commit"' })
-      
-        if (newBranchName) {
-          await runGitCommand({ targetFolder, command: `branch ${newBranchName}` })
-          await runGitCommand({ targetFolder, command: `checkout ${newBranchName}` })
-          gitSpinner.success(`Git repository initialized, initial commit created and new branch ${newBranchName} created`)
-        } else {
-          gitSpinner.success('Git repository initialized and initial commit created')
+
+      const gitFolderPath = join(targetFolder, '.git')
+
+      if (!existsSync(gitFolderPath)) {
+        try {
+          await runGitCommand({ targetFolder, command: 'init' })
+          await runGitCommand({ targetFolder, command: 'add .' })
+          await runGitCommand({ targetFolder, command: 'commit -m "Initial commit"' })
+        
+          if (newBranchName) {
+            await runGitCommand({ targetFolder, command: `branch ${newBranchName}` })
+            await runGitCommand({ targetFolder, command: `checkout ${newBranchName}` })
+            gitSpinner.success(`Git repository initialized, initial commit created and new branch ${newBranchName} created`)
+          } else {
+            gitSpinner.success('Git repository initialized and initial commit created')
+          }
+        
+        } catch (e) {
+          console.error(e)
+          gitSpinner.error('Failed to initialize git repository or create branch')
+          process.exit(1)
         }
-      
-      } catch (e) {
-        console.error(e)
-        gitSpinner.error('Failed to initialize git repository or create branch')
-        process.exit(1)
+      } else {
+        gitSpinner.warning('Git repository already initialized, skipping git init')
       }
     } else {
       console.log(`${colors.yellow('Skipped')} initializing git repository`)
