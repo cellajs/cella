c131a3a0b2ad4299856f3a4ea7443526c959a233
David Hordiienko <shiraz0david@gmail.com>
2024-09-27 18:16:39 +0300

    fixes

--- frontend/src/modules/projects/board/header/board-search.tsx
+++ frontend/src/modules/projects/board/header/board-search.tsx
@@ -1,5 +1,5 @@
 import { Search, XCircle } from 'lucide-react';
-import { useContext, useEffect, useMemo, useRef } from 'react';
+import { type MouseEventHandler, useContext, useEffect, useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import useSaveInSearchParams from '~/hooks/use-save-in-search-params';
 import { TableFilterBarContext } from '~/modules/common/data-table/table-filter-bar';
@@ -25,14 +25,19 @@ const BoardSearch = ({ toggleFocus }: { toggleFocus: () => void }) => {
   );
   useSaveInSearchParams(filters);
 
+  const preventInputBlur: MouseEventHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
   // Focus input when filter button clicked(mobile)
   useEffect(() => {
     if (isFilterActive) inputRef.current?.focus();
   }, [isFilterActive]);
 
   return (
-    <div className="relative flex w-full sm:min-w-44 items-center" onClick={handleClick} onKeyDown={undefined}>
-      <Search size={16} className="absolute left-3" style={{ opacity: `${searchQuery.length ? 1 : 0.5}` }} />
+    <div className="relative flex w-full sm:min-w-44 items-center" onClick={handleClick} onFocus={toggleFocus} onKeyDown={undefined}>
+      <Search size={16} onMouseDown={preventInputBlur} className="absolute left-3" style={{ opacity: `${searchQuery.length ? 1 : 0.5}` }} />
       <Input
         onFocus={toggleFocus}
         onBlur={toggleFocus}
@@ -51,7 +56,10 @@ const BoardSearch = ({ toggleFocus }: { toggleFocus: () => void }) => {
         <XCircle
           size={16}
           className="absolute right-4 top-1/2 opacity-70 hover:opacity-100 -translate-y-1/2 cursor-pointer"
-          onClick={() => setSearchQuery('')}
+          onMouseDown={(e) => {
+            preventInputBlur(e);
+            setSearchQuery('');
+          }}
         />
       )}
     </div>
