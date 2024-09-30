38efd278bc799c6073dc6655f712608b9e171be5
flipvanhaaren <flip@shareworks.nl>
2024-09-30 11:52:07 +0200

    vaul update

--- frontend/src/modules/common/sheeter/drawer.tsx
+++ frontend/src/modules/common/sheeter/drawer.tsx
@@ -1,10 +1,10 @@
-import { cn } from '~/lib/utils';
 import type { SheetProp } from '~/modules/common/sheeter/sheet';
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from '~/modules/ui/drawer';
+import { cn } from '~/utils/utils';
 
 export default function MobileSheet({ title, description, content, className, onOpenChange }: SheetProp) {
   return (
-    <Drawer open={true} onOpenChange={onOpenChange}>
+    <Drawer open={true} direction="right" noBodyStyles onOpenChange={onOpenChange}>
       <DrawerContent className={cn('min-h-[60vh]', className)}>
         <DrawerHeader className={`${description || title ? '' : 'hidden'}`}>
           <DrawerTitle className={`font-medium mb-2 ${title ? '' : 'hidden'}`}>{title}</DrawerTitle>
--- frontend/src/modules/common/sheeter/index.tsx
+++ frontend/src/modules/common/sheeter/index.tsx
@@ -2,9 +2,9 @@ import { useNavigate } from '@tanstack/react-router';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { useBreakpoints } from '~/hooks/use-breakpoints';
-import { objectKeys } from '~/lib/object';
 import { dialog } from '~/modules/common/dialoger/state';
 import { type SheetAction, SheetObserver, type SheetT, sheet } from '~/modules/common/sheeter/state';
+import { objectKeys } from '~/utils/object';
 import MobileSheet from './drawer';
 import DesktopSheet from './sheet';
 
