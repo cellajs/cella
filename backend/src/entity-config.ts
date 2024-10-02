d8f67ba70d790f5d07c050732af8dd9af73bac05
flipvanhaaren <flip@shareworks.nl>
2024-10-02 22:23:01 +0200

    use entity config to build usermenu schema

--- backend/src/entity-config.ts
+++ backend/src/entity-config.ts
@@ -5,13 +5,15 @@ export type EntityTables = (typeof entityTables)[keyof typeof entityTables];
 
 export type EntityTableNames = EntityTables['_']['name'];
 
+export type StorageType = (typeof entityMenuSections)[number]['storageType'];
+
 // Define what are the entities and their tables
 export const entityTables = {
   user: usersTable,
   organization: organizationsTable,
 } as const;
 
-// Define how entities are renderd in user menu
+// Define how entities are rendered in user menu
 export const entityMenuSections = [
   {
     storageType: 'organizations' as const,
@@ -19,3 +21,6 @@ export const entityMenuSections = [
     isSubmenu: false,
   },
 ];
+
+// Expose unique storage types for menu schema
+export const uniqueStorageTypes = Array.from(new Set(entityMenuSections.map((section) => section.storageType)));
