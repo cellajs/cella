d93303061bc39c14700576c63474a9e679552fa5
flipvanhaaren <flip@shareworks.nl>
2024-10-09 14:41:29 +0200

    handle ESC and click outside for sheet with nav sheet in mind

--- frontend/src/modules/common/sheeter/sheet.tsx
+++ frontend/src/modules/common/sheeter/sheet.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import StickyBox from '~/modules/common/sticky-box';
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '~/modules/ui/sheet';
 import { type SheetT, sheet as sheetState } from './state';
@@ -8,6 +9,8 @@ export interface SheetProp {
 
 export default function DesktopSheet({ sheet, removeSheet }: SheetProp) {
   const { id, modal = true, side = 'right', open, description, title, hideClose = true, className, content } = sheet;
+  const sheetRef = useRef<HTMLDivElement>(null);
+
   const closeSheet = () => {
     removeSheet(sheet);
     sheet.removeCallback?.();
@@ -19,10 +22,28 @@ export default function DesktopSheet({ sheet, removeSheet }: SheetProp) {
     if (!open) closeSheet();
   };
 
+  const handleEscapeKeyDown = (e: KeyboardEvent) => {
+    const activeElement = document.activeElement;
+    // Don't close sheet if not in modal and active element is not in sheet
+    if (!modal && !sheetRef.current?.contains(activeElement)) return;
+    e.preventDefault();
+    closeSheet();
+  };
+
+  // Close sheet if clicked outside and not in modal
+  const handleInteractOutside = (event: CustomEvent<{ originalEvent: PointerEvent }> | CustomEvent<{ originalEvent: FocusEvent }>) => {
+    const mainContentElement = document.getElementById('main-block-app-content');
+    if (!modal && mainContentElement?.contains(event.target as Node)) {
+      return closeSheet();
+    }
+  };
+
   return (
     <Sheet open={open} onOpenChange={onOpenChange} modal={modal}>
       <SheetContent
-        onEscapeKeyDown={closeSheet}
+        ref={sheetRef}
+        onEscapeKeyDown={handleEscapeKeyDown}
+        onInteractOutside={handleInteractOutside}
         side={side}
         hideClose={hideClose}
         aria-describedby={undefined}
