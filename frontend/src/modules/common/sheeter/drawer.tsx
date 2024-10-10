5d766a4792394d4ad6ca6a8e0998f6d00c1f9fb1
flipvanhaaren <flip@shareworks.nl>
2024-10-10 23:23:46 +0200

    fix sheet / drawer

--- frontend/src/modules/common/sheeter/drawer.tsx
+++ frontend/src/modules/common/sheeter/drawer.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import type { SheetProp } from '~/modules/common/sheeter/sheet';
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from '~/modules/ui/drawer';
 import { sheet as sheetState } from './state';
 
 export default function MobileSheet({ sheet, removeSheet }: SheetProp) {
-  const { modal = true, side, description, title, className, content, open } = sheet;
+  const { modal = true, side: sheetSide, description, title, className: sheetClassName, content, open } = sheet;
+
+  // State to retain side value even after sheet removal
+  const [side, setSide] = useState(sheetSide);
+  const [className, setClassName] = useState(sheetClassName);
+
+  // Prevent flickering of sheet when its removed
+  useEffect(() => {
+    if (sheetSide) {
+      setSide(sheetSide); // Update side when new sheet is created
+      setClassName(sheetClassName);
+    }
+  }, [sheetSide, sheetClassName]);
 
   const closeSheet = () => {
     removeSheet(sheet);
--- frontend/src/modules/common/sheeter/sheet.tsx
+++ frontend/src/modules/common/sheeter/sheet.tsx
@@ -1,15 +1,28 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import StickyBox from '~/modules/common/sticky-box';
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '~/modules/ui/sheet';
 import { type SheetT, sheet as sheetState } from './state';
+
 export interface SheetProp {
   sheet: SheetT;
   removeSheet: (sheet: SheetT) => void;
 }
 
 export default function DesktopSheet({ sheet, removeSheet }: SheetProp) {
-  const { id, modal = true, side, open, description, title, hideClose = true, className, content } = sheet;
+  const { id, modal = true, side: sheetSide, open, description, title, hideClose = true, className: sheetClassName, content } = sheet;
   const sheetRef = useRef<HTMLDivElement>(null);
+
+  // State to retain side value even after sheet removal
+  const [side, setSide] = useState(sheetSide);
+  const [className, setClassName] = useState(sheetClassName);
+
+  // Prevent flickering of sheet when its removed
+  useEffect(() => {
+    if (sheetSide) {
+      setSide(sheetSide); // Update side when new sheet is created
+      setClassName(sheetClassName);
+    }
+  }, [sheetSide, sheetClassName]);
 
   const closeSheet = () => {
     removeSheet(sheet);
@@ -24,14 +37,12 @@ export default function DesktopSheet({ sheet, removeSheet }: SheetProp) {
 
   const handleEscapeKeyDown = (e: KeyboardEvent) => {
     const activeElement = document.activeElement;
-    // Don't close sheet if not in modal and active element is not in sheet
     if (!modal && !sheetRef.current?.contains(activeElement)) return;
     e.preventDefault();
     e.stopPropagation();
     closeSheet();
   };
 
-  // Close sheet if clicked outside and not in modal
   const handleInteractOutside = (event: CustomEvent<{ originalEvent: PointerEvent }> | CustomEvent<{ originalEvent: FocusEvent }>) => {
     const bodyClassList = document.body.classList;
     if (bodyClassList.contains('keep-menu-open') && bodyClassList.contains('menu-sheet-open')) return;
@@ -48,7 +59,7 @@ export default function DesktopSheet({ sheet, removeSheet }: SheetProp) {
         ref={sheetRef}
         onEscapeKeyDown={handleEscapeKeyDown}
         onInteractOutside={handleInteractOutside}
-        side={side}
+        side={side} // Retained side value
         hideClose={hideClose}
         aria-describedby={undefined}
         className={`${className} items-start`}
