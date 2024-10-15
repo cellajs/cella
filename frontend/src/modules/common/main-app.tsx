149c3b7860351dfb68beafac5f7edc4465719092
David Hordiienko <shiraz0david@gmail.com>
2024-10-15 10:19:44 +0300

    add: usage of title hook

--- frontend/src/modules/common/main-app.tsx
+++ frontend/src/modules/common/main-app.tsx
@@ -1,6 +1,7 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import { MainContent } from '~/modules/common/main-content';
 
+import { useSetDocumentTitle } from '~/hooks/use-set-document-title';
 import { Dialoger } from '~/modules/common/dialoger';
 import { DropDowner } from '~/modules/common/dropdowner';
 import ErrorNotice from '~/modules/common/error-notice';
@@ -11,6 +12,7 @@ import SSE from '~/modules/common/sse';
 import { SSEProvider } from '~/modules/common/sse/provider';
 
 const App = () => {
+  useSetDocumentTitle();
   return (
     <div id="main-app-root">
       <ErrorBoundary
