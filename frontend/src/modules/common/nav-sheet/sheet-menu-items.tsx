2728d9a0b7edc36e9efae7b665d723702c782a86
David Hordiienko <shiraz0david@gmail.com>
2024-10-18 12:16:21 +0300

    +

--- frontend/src/modules/common/nav-sheet/sheet-menu-items.tsx
+++ frontend/src/modules/common/nav-sheet/sheet-menu-items.tsx
@@ -26,12 +26,11 @@ export const SheetMenuItem = ({ item, className, searchResults }: SheetMenuItemP
 
   return (
     <Link
-      data-search-results={!!searchResults}
-      data-submenu={!!item.submenu}
+      data-regular-item={!searchResults && !item.submenu}
       data-active={isActive}
       resetScroll={false}
       className={cn(
-        'group flex h-14 w-full flex my-1 cursor-pointer items-start justify-start space-x-1 rounded p-0 focus:outline-none ring-2 ring-inset ring-transparent focus:ring-foreground hover:bg-accent/50 hover:text-accent-foreground data-[submenu=false]:data-[search-results=false]:h-12 data-[submenu=false]:data-[search-results=false]:relative data-[submenu=false]:data-[search-results=false]:menu-item-sub data-[active=true]:ring-transparent data-[active=true]:bg-accent ',
+        'group/menuItem flex h-14 w-full flex my-1 cursor-pointer items-start justify-start space-x-1 rounded p-0 focus:outline-none ring-2 ring-inset ring-transparent focus:ring-foreground hover:bg-accent/50 hover:text-accent-foreground data-[regular-item=true]:h-12 data-[regular-item=true]:relative data-[regular-item=true]:menu-item-sub data-[active=true]:ring-transparent data-[active=true]:bg-accent',
         className,
       )}
       aria-label={item.name}
@@ -39,7 +38,7 @@ export const SheetMenuItem = ({ item, className, searchResults }: SheetMenuItemP
       params={{ idOrSlug, orgIdOrSlug }}
     >
       <AvatarWrap
-        className={`${!item.submenu && !searchResults ? 'my-2 mx-3 h-8 w-8 text-xs' : 'm-2'} z-[1] items-center`}
+        className="z-[1] items-center m-2 group-data-[regular-item=true]/menuItem:my-2 group-data-[regular-item=true]/menuItem:mx-3 group-data-[regular-item=true]/menuItem:text-xs group-data-[regular-item=true]/menuItem:h-8 group-data-[regular-item=true]/menuItem:w-8"
         type={item.entity}
         id={item.id}
         name={item.name}
@@ -47,13 +46,19 @@ export const SheetMenuItem = ({ item, className, searchResults }: SheetMenuItemP
       />
       <div className="truncate grow py-2 flex flex-col justify-center pr-2 text-left">
         <div
-          className={`truncate ${!item.submenu && !searchResults ? 'max-sm:pt-1.5 text-sm sm:-mb-1 sm:-mt-0.5' : 'max-sm:pt-2.5 text-base'} leading-5`}
+          className="truncate leading-5 max-sm:pt-2.5 text-base group-data-[regular-item=true]/menuItem:text-sm
+            group-data-[regular-item=true]/menuItem:max-sm:pt-1.5 group-data-[regular-item=true]/menuItem:sm:-mb-1
+            group-data-[regular-item=true]/menuItem:sm:-mt-0.5"
         >
           {item.name}
         </div>
-        <div className={`max-sm:hidden text-muted-foreground ${!item.submenu && !searchResults ? 'text-xs mt-0.5' : 'text-sm'} font-light`}>
-          {searchResults && <span className="inline transition-all duration-500 ease-in-out group-hover:hidden ">{t(item.entity)}</span>}
-          <span className="hidden transition-all duration-500 ease-in-out group-hover:inline ">
+        <div
+          className="max-sm:hidden text-muted-foreground text-sm font-light
+          group-data-[regular-item=true]/menuItem:sm:text-xs
+          group-data-[regular-item=true]/menuItem:sm:-mt-0.5"
+        >
+          {searchResults && <span className="inline transition-all duration-500 ease-in-out group-hover/menuItem:hidden ">{t(item.entity)}</span>}
+          <span className="hidden transition-all duration-500 ease-in-out group-hover/menuItem:inline ">
             {item.submenu?.length
               ? `${item.submenu?.length} ${t(`app:${item.submenu?.length > 1 ? `${item.submenu[0].entity}s` : item.submenu[0].entity}`).toLowerCase()}`
               : item.membership.role
