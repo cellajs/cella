8f666caa5a567838e196ea3ed297e469558086bf
Roman <nabukhotnyiroman@gmail.com>
2024-11-14 20:07:28 +0200

    fix: Fix imports loop

--- backend/src/db/lucia.ts
+++ backend/src/db/lucia.ts
@@ -4,28 +4,19 @@ import { config } from 'config';
 import { Lucia, type SessionCookieOptions, TimeSpan } from 'lucia';
 
 import { db } from '#/db/db';
-import authRoutesConfig from '#/modules/auth/routes';
 import { env } from '../../env';
 import { type SessionModel, sessionsTable } from './schema/sessions';
 import { type UnsafeUserModel, usersTable } from './schema/users';
 
-export const githubAuth = new GitHub(
-  env.GITHUB_CLIENT_ID || '',
-  env.GITHUB_CLIENT_SECRET || '',
-  config.backendAuthUrl + authRoutesConfig.githubSignInCallback.path,
-);
+export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID || '', env.GITHUB_CLIENT_SECRET || '', `${config.backendAuthUrl}/github/callback`);
 
-export const googleAuth = new Google(
-  env.GOOGLE_CLIENT_ID || '',
-  env.GOOGLE_CLIENT_SECRET || '',
-  config.backendAuthUrl + authRoutesConfig.googleSignInCallback.path,
-);
+export const googleAuth = new Google(env.GOOGLE_CLIENT_ID || '', env.GOOGLE_CLIENT_SECRET || '', `${config.backendAuthUrl}/google/callback`);
 
 export const microsoftAuth = new MicrosoftEntraId(
   env.MICROSOFT_TENANT_ID || '',
   env.MICROSOFT_CLIENT_ID || '',
   env.MICROSOFT_CLIENT_SECRET || '',
-  config.backendAuthUrl + authRoutesConfig.microsoftSignInCallback.path,
+  `${config.backendAuthUrl}/microsoft/callback`,
 );
 
 // Create Lucia adapter instance
