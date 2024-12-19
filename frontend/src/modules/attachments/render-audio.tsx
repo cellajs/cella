418ef7b10610bfafb1dfd50bc7ab4eeebb9e93d2
flipvanhaaren <flip@shareworks.nl>
2024-12-19 14:56:35 +0100

    refactor: lazy load attachment code due to bundle size

--- frontend/src/modules/attachments/render-audio.tsx
+++ frontend/src/modules/attachments/render-audio.tsx
@@ -1,8 +1,10 @@
 import MediaThemeSutroAudio from 'player.style/sutro-audio/react';
 
-export const RenderAudio = ({ src }: { src: string }) => (
+const RenderAudio = ({ src }: { src: string }) => (
   <MediaThemeSutroAudio className="w-[70%] p-2 max-h-10">
     {/* biome-ignore lint/a11y/useMediaCaption: by author */}
     <audio slot="media" src={src} playsInline crossOrigin="anonymous" />
   </MediaThemeSutroAudio>
 );
+
+export default RenderAudio;
