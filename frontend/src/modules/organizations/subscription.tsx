8b17e30cbec4d2c17087801b2b019ebc19a2791c
David Hordiienko <shiraz0david@gmail.com>
2024-12-19 11:47:10 +0200

    improve: Subscription checkout on offline

--- frontend/src/modules/organizations/subscription.tsx
+++ frontend/src/modules/organizations/subscription.tsx
@@ -1,7 +1,9 @@
 import { type Paddle, initializePaddle } from '@paddle/paddle-js';
+import { onlineManager } from '@tanstack/react-query';
 import { config } from 'config';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { showToast } from '~/lib/toasts';
 import { Button } from '~/modules/ui/button';
 import type { Organization } from '~/types/common';
 
@@ -15,6 +17,8 @@ const Subscription = ({ organization }: { organization: Organization }) => {
 
   // Callback to open a checkout
   const openCheckout = (priceId: string) => {
+    if (!onlineManager.isOnline()) return showToast(t('common:action.offline.text'), 'warning');
+
     paddle?.Checkout.open({
       items: [{ priceId, quantity: 1 }],
     });
