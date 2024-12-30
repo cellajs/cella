811b323c351b350a8796518ae7e3ff03fd0722ab
David Hordiienko <shiraz0david@gmail.com>
2024-12-30 14:57:43 +0200

    add: before load show user from cash

--- frontend/src/modules/users/helpers.tsx
+++ frontend/src/modules/users/helpers.tsx
@@ -2,13 +2,16 @@ import { config } from 'config';
 import { t } from 'i18next';
 
 import { decodeBase64, encodeBase64 } from '@oslojs/encoding';
-import { onlineManager } from '@tanstack/react-query';
+import { type QueryKey, onlineManager } from '@tanstack/react-query';
 import { toast } from 'sonner';
 import { authThroughPasskey, getChallenge, setPasskey } from '~/api/auth';
 import { deletePasskey as baseRemovePasskey, getSelf, getUserMenu } from '~/api/me';
 import { showToast } from '~/lib/toasts';
+import { getQueryItems } from '~/query/helpers/mutate-query';
+import type { InfiniteQueryData, QueryData } from '~/query/types';
 import { useNavigationStore } from '~/store/navigation';
 import { useUserStore } from '~/store/user';
+import type { LimitedUser } from '~/types/common';
 
 // Register a new passkey
 export const registerPasskey = async () => {
@@ -137,4 +140,15 @@ export const getAndSetMenu = async () => {
   const menu = await getUserMenu();
   useNavigationStore.setState({ menu });
   return menu;
+};
+
+export const findUserFromQueries = (queries: [QueryKey, InfiniteQueryData<LimitedUser> | QueryData<LimitedUser> | undefined][], idOrSlug: string) => {
+  for (const [_, prevData] of queries) {
+    if (!prevData) continue;
+
+    const data = getQueryItems(prevData);
+    const user = data.find((item) => item.id === idOrSlug);
+    if (user) return user;
+  }
+  return null;
 };
