1cb3eeea436f89a82153e8fba7bf02d675b1942d
David Hordiienko <shiraz0david@gmail.com>
2025-02-06 14:40:02 +0200

    refactor: make tokens table dynamic & add ref to tokensTable from membershipsTable

---
+++ backend/src/db/types.ts
@@ -0,0 +1,21 @@
+import type { PgColumn, PgVarcharBuilderInitial } from 'drizzle-orm/pg-core';
+import type { ContextEntityIdFields } from '#/entity-config';
+
+export type DynamicColumn = PgColumn<{
+  name: ContextEntityIdFields;
+  tableName: string;
+  dataType: 'string';
+  columnType: 'PgVarchar';
+  data: string;
+  driverParam: string;
+  notNull: false;
+  hasDefault: false;
+  isPrimaryKey: false;
+  isAutoincrement: false;
+  hasRuntimeDefault: false;
+  enumValues: [string, ...string[]];
+  baseColumn: never;
+  generated: undefined;
+}>;
+
+export type ContextEntityColumns = Record<ContextEntityIdFields, PgVarcharBuilderInitial<'', [string, ...string[]], undefined>>;
