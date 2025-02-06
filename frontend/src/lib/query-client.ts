0ed5ebcf3e6f99a482ac87302d6da110cc5da3c8
David Hordiienko <shiraz0david@gmail.com>
2025-02-06 12:16:42 +0200

    chore: load of organization info in routes

--- frontend/src/lib/query-client.ts
+++ frontend/src/lib/query-client.ts
@@ -85,51 +85,52 @@ const onSuccess = () => {
 };
 
 /**
- * Function to fetch data. If online, fetches the query even if there is cached. If offline or an error occurs, it tries to get the cached data.
+ * Function to fetch data with offline support. If online, it will attempt to fetch data and perform a background revalidation.
+ * If offline, it will attempt to return cached data. If there is an error during the fetch,
+ * it will fall back to cached data if available.
  *
- * @param options - Fetch query options that define the query behavior and parameters.
- * @returns Returns query data of  undefined.
+ * @param options - Fetch query options that define the query behavior, including the query key and parameters.
+ * @param refetchIfOnline - Optional, flag to control refetching online (default: `true`).
+ * @returns Returns query data or undefined.
  */
 // biome-ignore lint/suspicious/noExplicitAny: any is used to infer the type of the options
-export const offlineFetch = async (options: FetchQueryOptions<any, any, any, any>) => {
+export const offlineFetch = async <T>(options: FetchQueryOptions<any, any, any, any>, refetchIfOnline = true): Promise<T | undefined> => {
   const { queryKey } = options;
-  const cachedData = queryClient.getQueryData(queryKey);
+  const cachedData = queryClient.getQueryData<T>(queryKey);
 
-  // If offline, return cached data or undefined if no cache exists
+  // If offline, return cached data if available
   if (!onlineManager.isOnline()) return cachedData ?? undefined;
 
   try {
-    // If online, fetch data (background revalidation)
-    queryClient.removeQueries({ queryKey, exact: true });
+    // Remove cached queries to trigger re-fetch if online
+    if (refetchIfOnline) queryClient.removeQueries({ queryKey, exact: true });
     return queryClient.fetchQuery(options);
   } catch (error) {
-    // Fallback to cached data if available
-    return cachedData ?? undefined;
+    return cachedData ?? undefined; // Fallback to cached data if available
   }
 };
 
 /**
  * Function to fetch infinite data. If online, fetches the query even if there is cached. If offline or an error occurs, it tries to get the cached data.
  *
- * @param options - Fetch infinite query options that define the query behavior and parameters, including the query key and other settings.
- * @returns Returns query data of  undefined.
- *
+ * @param options - Fetch infinite query options that define the query behavior and parameters,
+ * including the query key and other settings.
+ * @param refetchIfOnline - Optional, flag to control refetching online (default: `true`).
+ * @returns Returns query data or undefined.
  */
 // biome-ignore lint/suspicious/noExplicitAny: any is used to infer the type of the options
-export const offlineFetchInfinite = async (options: FetchInfiniteQueryOptions<any, any, any, any, any>) => {
+export const offlineFetchInfinite = async (options: FetchInfiniteQueryOptions<any, any, any, any, any>, refetchIfOnline = true) => {
   const { queryKey } = options;
   const cachedData = queryClient.getQueryData(queryKey);
 
-  // If offline, return cached data or undefined if no cache exists
+  // If offline, return cached data if available
   if (!onlineManager.isOnline()) return cachedData ?? undefined;
 
   try {
-    // If online, fetch data (background revalidation)
-    queryClient.removeQueries({ queryKey, exact: true });
+    if (refetchIfOnline) queryClient.removeQueries({ queryKey, exact: true });
     return queryClient.fetchInfiniteQuery(options);
   } catch (error) {
-    // Fallback to cached data if available
-    return cachedData ?? undefined;
+    return cachedData ?? undefined; // Fallback to cached data if available
   }
 };
 
