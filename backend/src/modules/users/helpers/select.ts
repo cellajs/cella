5d5f3e4640d770a6cf1b205fd87024b051ed53cd
David Hordiienko <shiraz0david@gmail.com>
2025-02-11 17:26:29 +0200

    create: limitUserSelect

--- backend/src/modules/users/helpers/select.ts
+++ backend/src/modules/users/helpers/select.ts
@@ -1,8 +1,13 @@
 import { config } from 'config';
 import { usersTable } from '#/db/schema/users';
-import { omitKeys } from '#/utils/omit';
+import { extractKeys, omitKeys } from '#/utils/schema-tools';
 
 /**
  * Safe user select. Sensitive fields are omitted.
  */
 export const userSelect = omitKeys(usersTable, config.sensitiveFields);
+
+/**
+ * Limited user select. Include min info.
+ */
+export const limitUserSelect = extractKeys(usersTable, ['id', 'name', 'email', 'entity', 'thumbnailUrl', 'bannerUrl']);
--- backend/src/utils/schema-tools.ts
+++ backend/src/utils/schema-tools.ts
@@ -14,3 +14,20 @@ export const omitKeys = <T extends TableConfig, K extends keyof T['columns']>(ob
   }
   return newObj;
 };
+
+/**
+ * Returns a table select that include only specified columns.
+ *
+ * @param obj - drizzle orm's pgTable.
+ * @param keys - Columns to select from the table configuration.
+ * @returns A new table configuration with specified columns.
+ */
+export const extractKeys = <T extends TableConfig, K extends keyof T['columns']>(obj: PgTableWithColumns<T>, keys: K[] | readonly K[]) => {
+  const extractedObj = {} as Pick<PgTableWithColumns<T>, K>;
+
+  for (const key of keys) {
+    if (key in obj) extractedObj[key] = obj[key];
+  }
+
+  return extractedObj;
+};
