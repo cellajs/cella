cf9e2b80719d42c4f04584510e609d74b0ad6222
David Hordiienko <shiraz0david@gmail.com>
2025-02-12 16:21:39 +0200

    improve: comments in db utils

--- backend/src/db/utils.ts
+++ backend/src/db/utils.ts
@@ -3,8 +3,15 @@ import { type PgColumnBuilderBase, pgTable, varchar } from 'drizzle-orm/pg-core'
 import type { ContextEntityColumns } from '#/db/types';
 import { entityIdFields, entityTables } from '#/entity-config';
 
-// TODO improve comments
-// Generate table with type-safe columns
+/**
+ * Utility function to generate a drizzle pgTable with type-safe columns.
+ * Combines base columns and additional columns to define a table structure dynamically.
+ *
+ * @param  tableName - Name of the table.
+ * @param  baseColumns - The base columns shared across tables.
+ * @param  additionalColumns - The additional columns specific to this table.
+ * @returns  - The generated database table.
+ */
 export const generateTable = <
   TBaseColumns extends Record<string, PgColumnBuilderBase>,
   TAdditionalColumns extends Record<string, PgColumnBuilderBase>,
@@ -18,12 +25,19 @@ export const generateTable = <
     ...additionalColumns,
   });
 
-// Helper function for fields generation based of context entities
+/**
+ * Helper function to generate fields dynamically based on `config.contextEntityTypes`,
+ * loops through and create references for each entity type, ensuring proper relational mapping.
+ *
+ * @returns A set of dynamically generated fields for context entities.
+ */
 export const generateContextEntityFields = () =>
   config.contextEntityTypes.reduce((fields, entityType) => {
-    const fieldTable = entityTables[entityType];
-    const fieldName = entityIdFields[entityType];
-    // Add the field with optional constraints
+    const fieldTable = entityTables[entityType]; // Retrieve associated table for entity
+    const fieldName = entityIdFields[entityType]; // Determine the entity's ID field name
+
+    // Add the field with a foreign key reference, ensuring cascading deletion
     fields[fieldName] = varchar().references(() => fieldTable.id, { onDelete: 'cascade' });
+
     return fields;
   }, {} as ContextEntityColumns);
