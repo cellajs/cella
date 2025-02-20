440bf66eab5b8c6abbbc2aeed8d0fa891a1b33b2
David Hordiienko <shiraz0david@gmail.com>
2025-02-20 10:49:34 +0200

    chore: Review type assertion (as 400)

--- backend/src/utils/zod.ts
+++ backend/src/utils/zod.ts
@@ -11,3 +11,17 @@ export function constructZodLiteralUnionType<T extends z.ZodLiteral<unknown>>(li
 
   return z.union(literals);
 }
+
+export function numberEnum<T extends number>(values: readonly T[]) {
+  const set = new Set<unknown>(values);
+  return (v: number, ctx: z.RefinementCtx): v is T => {
+    if (!set.has(v)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_enum_value,
+        received: v,
+        options: [...values],
+      });
+    }
+    return z.NEVER;
+  };
+}
