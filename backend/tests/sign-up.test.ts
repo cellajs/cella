56432d3e4c246592e673c31d4e35aa7317eee704
David Hordiienko <shiraz0david@gmail.com>
2025-02-21 14:53:56 +0200

    improve: sign-up test check whole flow with email verify

--- backend/tests/sign-up.test.ts
+++ backend/tests/sign-up.test.ts
@@ -1,9 +1,10 @@
 import path from 'node:path';
 import { config } from 'config';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 import { migrate } from 'drizzle-orm/pglite/migrator';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import { db } from '../src/db/db';
+import { tokensTable } from '../src/db/schema/tokens';
 import { usersTable } from '../src/db/schema/users';
 import routes from '../src/routes';
 
@@ -16,7 +17,7 @@ beforeAll(async () => await migrate(db, { migrationsFolder: path.resolve(process
 // Clean up test data after all tests
 afterAll(async () => await db.delete(usersTable).where(eq(usersTable.email, 'test@gmail.com')));
 
-describe('sign-up', () => {
+describe('Sign-up process', () => {
   // Sample test user details for sign-up tests
   const testUser = {
     email: 'test@gmail.com',
@@ -25,6 +26,7 @@ describe('sign-up', () => {
 
   const requestDetails = {
     method: 'POST',
+    credentials: 'include' as const,
     body: JSON.stringify(testUser),
     headers: { 'Content-Type': 'application/json', 'x-forwarded-for': '127.0.0.1' },
   };
@@ -67,15 +69,52 @@ describe('sign-up', () => {
   } else {
     // This block runs if all conditions are met for successful sign-up
     it('should successfully sign up a user when conditions are met', async () => {
-      const res = await routes.request('/auth/sign-up', requestDetails);
+      const signUp = await routes.request('/auth/sign-up', requestDetails);
 
-      expect(res.status).toBe(200);
-      const data = await res.json();
-      expect(data.success).toBe(true);
+      expect(signUp.status).toBe(200);
+      const { success: signUpSuccess } = await signUp.json(); // Parse response to check success flag
+      expect(signUpSuccess).toBe(true);
 
-      // Verify that the user is added to the database
+      // Verify that user was added to db
       const [user] = await db.select().from(usersTable).where(eq(usersTable.email, 'test@gmail.com'));
       expect(user).toBeDefined();
+
+      // Send a request to trigger email verification process
+      const tokenCreationRes = await routes.request('/auth/send-verification-email', {
+        ...requestDetails,
+        body: JSON.stringify({ userId: user.id }),
+      });
+
+      expect(tokenCreationRes.status).toBe(200);
+      const { success: tokenCreationSuccess } = await tokenCreationRes.json(); // Parse response to check success flag
+      expect(tokenCreationSuccess).toBe(true);
+
+      // Retrieve verification token from db
+      const [token] = await db
+        .select()
+        .from(tokensTable)
+        .where(and(eq(tokensTable.userId, user.id), eq(tokensTable.type, 'email_verification')));
+      expect(token).toBeDefined();
+
+      // Request to verify user's email using token
+      const emailVerifyRes = await routes.request(`/auth/verify-email/${token.token}`, requestDetails);
+
+      expect(emailVerifyRes.status).toBe(200);
+      const { success: emailVerifySuccess } = await emailVerifyRes.json(); // Parse response to check success flag
+      expect(emailVerifySuccess).toBe(true);
+
+      // Get Set-Cookie header from email verification response
+      const cookies = emailVerifyRes.headers.get('set-cookie');
+      expect(cookies).not.toBeNull();
+
+      if (cookies) {
+        const cookieName = `${config.slug}-session-${config.cookieVersion}`;
+        // Look for  session cookie with a dynamic name based on config
+        const sessionCookie = cookies.split(';').find((cookie) => cookie.startsWith(cookieName));
+        expect(sessionCookie).toBeDefined();
+
+        // TODO add  check token in table?
+      }
     });
 
     it('should throw an error if the user already exists', async () => {
