f4866609db0592b6ff4a2459deb3b67bdcc85f09
David Hordiienko <shiraz0david@gmail.com>
2025-03-21 16:49:24 +0200

    improve: attachment key factory

--- frontend/src/modules/attachments/query/options.ts
+++ frontend/src/modules/attachments/query/options.ts
@@ -1,4 +1,4 @@
-import { infiniteQueryOptions } from '@tanstack/react-query';
+import { infiniteQueryOptions, queryOptions } from '@tanstack/react-query';
 import { config } from 'config';
 
 import { type GetAttachmentsParams, getAttachments } from '~/modules/attachments/api';
@@ -10,12 +10,34 @@ import { getOffset } from '~/query/helpers';
  */
 export const attachmentsKeys = {
   all: ['attachments'] as const,
-  list: () => [...attachmentsKeys.all, 'list'] as const,
-  table: (filters?: GetAttachmentsParams) => [...attachmentsKeys.list(), filters] as const,
-  similar: (filters?: Pick<GetAttachmentsParams, 'orgIdOrSlug'>) => [...attachmentsKeys.list(), filters] as const,
+  list: {
+    base: () => [...attachmentsKeys.all, 'list'],
+    table: (filters: GetAttachmentsParams) => [...attachmentsKeys.list.base(), filters] as const,
+    tableByOrg: (filters: Pick<GetAttachmentsParams, 'orgIdOrSlug'>) => [...attachmentsKeys.list.base(), filters] as const,
+  },
   create: () => [...attachmentsKeys.all, 'create'] as const,
   update: () => [...attachmentsKeys.all, 'update'] as const,
   delete: () => [...attachmentsKeys.all, 'delete'] as const,
+};
+
+/**
+ * Query Options for fetching a grouped attachments.
+ *
+ * This function returns the configuration for querying group of attachments from target organization.
+ *
+ * @param param.orgIdOrSlug - Organization ID or slug.
+ * @param param.groupId - GroupId, of fetched attachments.
+ * @returns  Query options.
+ */
+export const groupedAttachmentsQueryOptions = ({ orgIdOrSlug, groupId }: Pick<GetAttachmentsParams, 'groupId' | 'orgIdOrSlug'>) => {
+  const queryKey = attachmentsKeys.list.base();
+
+  return queryOptions({
+    queryKey,
+    queryFn: () => getAttachments({ groupId, orgIdOrSlug }),
+    staleTime: 0,
+    gcTime: 0,
+  });
 };
 
 /**
@@ -35,20 +57,19 @@ export const attachmentsQueryOptions = ({
   q = '',
   sort: initialSort,
   order: initialOrder,
-  groupId,
   limit = config.requestLimits.attachments,
-}: GetAttachmentsParams) => {
+}: Omit<GetAttachmentsParams, 'groupId'>) => {
   const sort = initialSort || 'createdAt';
   const order = initialOrder || 'desc';
 
-  const queryKey = attachmentsKeys.table({ orgIdOrSlug, q, sort, order, groupId });
+  const queryKey = attachmentsKeys.list.table({ orgIdOrSlug, q, sort, order });
 
   return infiniteQueryOptions({
     queryKey,
     initialPageParam: 0,
     queryFn: async ({ pageParam: page, signal }) => {
       const offset = getOffset(queryKey); // Calculate before fetching ensuring correct offset
-      return await getAttachments({ page, q, sort, order, limit, groupId, orgIdOrSlug, offset }, signal);
+      return await getAttachments({ page, q, sort, order, limit, orgIdOrSlug, offset }, signal);
     },
     getNextPageParam: (_lastPage, allPages) => allPages.length,
   });
