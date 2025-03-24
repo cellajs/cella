85363415f03cbc380bddec562e50a94f0e2659fd
flipvanhaaren <flip@shareworks.nl>
2025-03-24 11:56:45 +0100

    fix: import typo

--- frontend/src/modules/attachments/render/pdf.tsx
+++ frontend/src/modules/attachments/render/pdf.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-import '~/modules/attachments/react-pdf.css';
+import '~/modules/attachments/render/react-pdf.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString();
 
