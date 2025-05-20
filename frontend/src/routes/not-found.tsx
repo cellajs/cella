ecfc32ae06341e821cc06bd786cdf829f04e01f6
flipvanhaaren <flip@shareworks.nl>
2025-05-20 11:16:16 +0200

    fix: typo

--- frontend/src/routes/not-found.tsx
+++ frontend/src/routes/not-found.tsx
@@ -4,7 +4,7 @@ import ErrorNotice from '~/modules/common/error-notice';
 import { rootRoute } from '~/routes/base';
 
 export const notFoundRoute = new NotFoundRoute({
-  staticData: { pageTitle: 'NotFound', isAuth: false },
+  staticData: { pageTitle: 'Not found', isAuth: false },
   getParentRoute: () => rootRoute,
   component: () => {
     return <ErrorNotice error={{ name: t('error:page_not_found'), message: t('error:page_not_found.text'), severity: 'info' }} level="root" />;
