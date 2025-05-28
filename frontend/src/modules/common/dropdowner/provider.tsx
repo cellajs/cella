afa58e58aa72584dc0f80ad4d37df7b33f804182
flipvanhaaren <flip@shareworks.nl>
2025-05-28 13:01:51 +0200

    fix: pull-to-refresh

--- frontend/src/modules/common/dropdowner/provider.tsx
+++ frontend/src/modules/common/dropdowner/provider.tsx
@@ -1,3 +1,4 @@
+import useBodyClass from '~/hooks/use-body-class';
 import { useBreakpoints } from '~/hooks/use-breakpoints';
 import DropdownDrawer from '~/modules/common/dropdowner/drawer';
 import { DesktopDropdown } from '~/modules/common/dropdowner/dropdown';
@@ -6,6 +7,9 @@ import { useDropdowner } from '~/modules/common/dropdowner/use-dropdowner';
 export function Dropdowner() {
   const dropdown = useDropdowner((state) => state.dropdown);
   const isMobile = useBreakpoints('max', 'sm');
+
+  // Apply body class
+  useBodyClass({ 'dropdowner-open': !!dropdown });
 
   if (!dropdown) return null;
   if (isMobile) return <DropdownDrawer dropdown={dropdown} />;
