bf3a94355d70aebadb6dff264cbfeed50e721b12
David Hordiienko <shiraz0david@gmail.com>
2025-06-17 12:14:19 +0300

    fix: types

--- frontend/src/modules/me/types.ts
+++ frontend/src/modules/me/types.ts
@@ -1,12 +1,11 @@
-import type { InferResponseType } from 'hono';
 import type { z } from 'zod';
-import type { client } from '~/modules/me/api';
 import type { User } from '~/modules/users/types';
+import type { GetMenuResponse } from '~/openapi-client';
 import type { meAuthDataSchema } from '#/modules/me/schema';
 
 export type Session = MeAuthData['sessions'][number];
 export type MeAuthData = z.infer<typeof meAuthDataSchema>;
 
 export type MeUser = User;
-export type UserMenu = Extract<InferResponseType<(typeof client.menu)['$get']>, { data: unknown }>['data'];
+export type UserMenu = GetMenuResponse['data'];
 export type UserMenuItem = UserMenu[keyof UserMenu][number];
