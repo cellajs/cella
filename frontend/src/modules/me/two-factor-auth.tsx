d93915477c62485395f6d1cbc984ca1daf7027e8
David Hordiienko <shiraz0david@gmail.com>
2025-09-03 10:24:56 +0300

    progress: add toast on 2fa toogle

--- frontend/src/modules/me/two-factor-auth.tsx
+++ frontend/src/modules/me/two-factor-auth.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import HelpText from '~/modules/common/help-text';
+import { toaster } from '~/modules/common/toaster/service';
 import { useUpdateSelfMutation } from '~/modules/me/query';
 import type { MeAuthData } from '~/modules/me/types';
 import { Switch } from '~/modules/ui/switch';
@@ -9,18 +10,28 @@ export const TwoFactorAuthentication = ({ userAuthData }: { userAuthData: MeAuth
   const { t } = useTranslation();
   const user = useUserStore((state) => state.user);
 
-  // TODO add success toast for disabling and enabling 2FA
-  const { mutateAsync: toggle2fa } = useUpdateSelfMutation();
+  const { mutateAsync: updateSelf } = useUpdateSelfMutation();
 
   const { hasPasskey } = userAuthData;
 
+  const toogle2fa = (twoFactorEnabled: boolean) => {
+    updateSelf(
+      { twoFactorEnabled },
+      {
+        onSuccess: () => {
+          const message = t(`2fa_${twoFactorEnabled ? 'enabled' : 'disabled'}`);
+          toaster(message, 'info');
+        },
+      },
+    );
+  };
   return (
     <>
       <HelpText content={t('common:2fa.text')}>
         <p className="font-semibold">{t('common:2fa')}</p>
       </HelpText>
       <div className="mb-6">
-        <Switch disabled={!hasPasskey} checked={user.twoFactorEnabled} onCheckedChange={(twoFactorEnabled) => toggle2fa({ twoFactorEnabled })} />
+        <Switch disabled={!hasPasskey} checked={user.twoFactorEnabled} onCheckedChange={toogle2fa} />
         {!hasPasskey && <p className="text-sm text-gray-500 mt-2">{t('common:2fa_disabled.text')}</p>}
       </div>
     </>
