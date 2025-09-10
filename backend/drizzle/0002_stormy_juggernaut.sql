7dc34f5dc7733f29e9aa9be77301b4553be0f97b
David Hordiienko <shiraz0david@gmail.com>
2025-09-10 11:03:39 +0300

    rebuild migrations

--- backend/drizzle/0002_stormy_juggernaut.sql
+++ backend/drizzle/0002_stormy_juggernaut.sql
@@ -7,6 +7,13 @@ CREATE TABLE "passwords" (
 	CONSTRAINT "passwords_userId_unique" UNIQUE("user_id")
 );
 --> statement-breakpoint
+CREATE TABLE "totps" (
+	"id" varchar PRIMARY KEY NOT NULL,
+	"user_id" varchar NOT NULL,
+	"encoder_secret_key" varchar NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "unsubscribe_tokens" (
 	"id" varchar PRIMARY KEY NOT NULL,
 	"user_id" varchar NOT NULL,
@@ -16,10 +23,17 @@ CREATE TABLE "unsubscribe_tokens" (
 	CONSTRAINT "unsubscribe_tokens_token_unique" UNIQUE("token")
 );
 --> statement-breakpoint
-ALTER TABLE "users" RENAME COLUMN "two_factor_enabled" TO "two_factor_required";--> statement-breakpoint
 ALTER TABLE "users" DROP CONSTRAINT "users_unsubscribeToken_unique";--> statement-breakpoint
 DROP INDEX "users_token_index";--> statement-breakpoint
+ALTER TABLE "passkeys" ADD COLUMN "device_name" varchar;--> statement-breakpoint
+ALTER TABLE "passkeys" ADD COLUMN "device_type" varchar DEFAULT 'desktop' NOT NULL;--> statement-breakpoint
+ALTER TABLE "passkeys" ADD COLUMN "device_os" varchar;--> statement-breakpoint
+ALTER TABLE "passkeys" ADD COLUMN "browser" varchar;--> statement-breakpoint
+ALTER TABLE "passkeys" ADD COLUMN "name_on_device" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "tokens" ADD COLUMN "consumed_at" timestamp with time zone;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "multi_factor_required" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "passwords" ADD CONSTRAINT "passwords_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "totps" ADD CONSTRAINT "totps_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "unsubscribe_tokens" ADD CONSTRAINT "unsubscribe_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "users_token_index" ON "unsubscribe_tokens" USING btree ("token");--> statement-breakpoint
 ALTER TABLE "users" DROP COLUMN "hashed_password";--> statement-breakpoint
--- backend/drizzle/0004_slimy_jimmy_woo.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "multi_factor_required" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "passkeys" DROP COLUMN "last_sign_in_at";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "two_factor_required";
