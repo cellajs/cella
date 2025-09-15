bc8f1c14a9cfdc3b19b7d83f5b79463d61a61032
David Hordiienko <shiraz0david@gmail.com>
2025-09-15 16:14:54 +0300

    add: alert in home as long as mfa is not enabled

--- frontend/src/modules/home/index.tsx
+++ frontend/src/modules/home/index.tsx
@@ -4,14 +4,20 @@ import { useTranslation } from 'react-i18next';
 import { getContextEntities } from '~/api.gen';
 import { SimpleHeader } from '~/modules/common/simple-header';
 import { EntityInvites } from '~/modules/me/entity-invitations';
+import { useAlertStore } from '~/store/alert';
+import { useUserStore } from '~/store/user';
 
 const Home = () => {
   const { t } = useTranslation();
+  const { setDownAlert } = useAlertStore();
 
   useEffect(() => {
     (async () => {
       getContextEntities();
     })();
+
+    const { user } = useUserStore.getState();
+    if (user.role === 'admin' && !user.mfaRequired && appConfig.mode === 'production') setDownAlert('enable_mfa');
   }, []);
   return (
     <div className="px-3 md:px-6">
