2bf0b61b58b0f0cf25e7fd21405d2a5d2cc863ec
flipvanhaaren <flip@shareworks.nl>
2025-10-08 12:12:41 +0200

    remove input number, not used

--- frontend/src/modules/common/form-fields/select-emails.tsx
+++ frontend/src/modules/common/form-fields/select-emails.tsx
@@ -39,6 +39,7 @@ export interface SelectEmailsProps {
   allowDuplicate?: boolean;
 }
 
+// TODO replace some of this with TagInput?
 export const SelectEmails = (props: SelectEmailsProps) => {
   const {
     id,
