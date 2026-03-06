6cfb07b0807ae7983054017cf04751885eb0cf85
flipvanhaaren <flip@shareworks.nl>
2026-03-06 21:13:41 +0100

    fix: cdc import file name mismatch

--- frontend/src/modules/common/data-grid/header-cell.tsx
+++ frontend/src/modules/common/data-grid/header-cell.tsx
@@ -14,7 +14,7 @@ import {
 } from './utils/grid-utils';
 
 const resizeHandleClassname =
-  'cursor-col-resize absolute inset-y-0 end-0 z-10 w-4 after:content-[""] after:absolute after:top-1/2 after:-translate-y-1/2 after:end-0 after:h-4 after:w-0.5 after:rounded-full after:bg-foreground/30 hover:after:bg-primary/80';
+  'cursor-col-resize absolute inset-y-0 end-0 w-4 after:content-[""] after:absolute after:top-1/2 after:-translate-y-1/2 after:end-0 after:h-4 after:w-0.5 after:rounded-full after:bg-foreground/30 hover:after:bg-primary/80';
 const dragImageClassname = 'rounded w-fit outline-2 outline-[hsl(207,100%,50%)] -outline-offset-2';
 
 type SharedHeaderRowProps<R, SR> = Pick<
